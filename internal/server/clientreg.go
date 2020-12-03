1c65988a5f8330846d4a777fde3b77f95b9cd84c
lhl2617 <lhlee888@hotmail.com>
2020-12-03 21:18:48 +0000

    fish!

--- internal/server/clientreg.go
+++ internal/server/clientreg.go
@@ -9,3 +9,10 @@ import (
 	_ "github.com/SOMAS2020/SOMAS2020/internal/clients/team5"
 	_ "github.com/SOMAS2020/SOMAS2020/internal/clients/team6"
 )
+
+//       /`·.¸
+//      /¸...¸`:·
+//  ¸.·´  ¸   `·.¸.·´)
+// : © ):´;      ¸  {
+//  `·.¸ `·  ¸.·´\`·¸)
+//      `\\´´\¸.·´
