e93cbb74dc86c639289e04c9a89ff18a1c8fcd04
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2020-12-11 19:34:57 +0100

    Update all clients to new model

--- internal/clients/team1/client.go
+++ internal/clients/team1/client.go
@@ -17,29 +17,17 @@ type client struct {
 	id shared.ClientID
 }
 
-/*
-func (c *client) Echo(s string) string {
-	c.logf("Echo: '%v'", s)
-	return s
-}
-
-func (c *client) GetID() shared.ClientID {
-	return c.id
-}
-
-// logf is the client's logger that prepends logs with your ID. This makes
-// it easier to read logs. DO NOT use other loggers that will mess logs up!
-func (c *client) logf(format string, a ...interface{}) {
-	log.Printf("[%v]: %v", c.id, fmt.Sprintf(format, a...))
-}
-
+// StartOfTurnUpdate is updates the gamestate of the client at the start of each turn.
+// The gameState is served by the server.
+// OPTIONAL. Base should be able to handle it but feel free to implement your own.
 func (c *client) StartOfTurnUpdate(gameState common.GameState) {
-	c.logf("Received game state update: %v", gameState)
+	c.Logf("Received game state update: %v", gameState)
 	// TODO
 }
 
+// EndOfTurnActions executes and returns the actions done by the client that turn.
+// OPTIONAL. Base should be able to handle it but feel free to implement your own.
 func (c *client) EndOfTurnActions() []common.Action {
-	c.logf("EndOfTurnActions")
+	c.Logf("EndOfTurnActions")
 	return nil
 }
-*/
