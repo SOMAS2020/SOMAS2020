fb2397cd921713345e3970e58599d50d73fd9568
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2020-12-11 19:18:08 +0100

    Add comments for clarity and lint

--- internal/common/base_client.go
+++ internal/common/base_client.go
@@ -9,34 +9,49 @@ import (
 
 const id = 420
 
+// NewClient produces a new client with the BaseClient already implemented.
+// BASE: Do not overwrite in team client.
 func NewClient(id shared.ClientID) Client {
 	return &BaseClient{id: id}
 }
 
+// BaseClient provides a basic implementation for all functions of the client interface and should always the interface fully.
+// All clients should be based off of this BaseClient to ensure that all clients implement the interface,
+// even when new features are added.
 type BaseClient struct {
 	id shared.ClientID
 }
 
+//Echo prints a message to show that the client exists
+// BASE: Do not overwrite in team client.
 func (c *BaseClient) Echo(s string) string {
 	c.logf("Echo: '%v'", s)
 	return s
 }
 
+// GetID gets returns the id of the client.
+// BASE: Do not overwrite in team client.
 func (c *BaseClient) GetID() shared.ClientID {
 	return c.id
 }
 
 // logf is the client's logger that prepends logs with your ID. This makes
 // it easier to read logs. DO NOT use other loggers that will mess logs up!
+// BASE: Do not overwrite in team client.
 func (c *BaseClient) logf(format string, a ...interface{}) {
 	log.Printf("[%v]: %v", c.id, fmt.Sprintf(format, a...))
 }
 
+// StartOfTurnUpdate is updates the gamestate of the client at the start of each turn.
+// The gameState is served by the server.
+// OPTIONAL. Base should be able to handle it but feel free to implement your own.
 func (c *BaseClient) StartOfTurnUpdate(gameState GameState) {
 	c.logf("Received game state update: %v", gameState)
 	// TODO
 }
 
+// EndOfTurnActions executes and returns the actions done by the client that turn.
+// OPTIONAL. Base should be able to handle it but feel free to implement your own.
 func (c *BaseClient) EndOfTurnActions() []Action {
 	c.logf("EndOfTurnActions")
 	return nil
