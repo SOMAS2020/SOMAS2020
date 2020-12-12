d4e56908374f5227a012a838bc734f9b6967342a
Ramon Jutaviriya <ramon15749@gmail.com>
2020-12-12 00:39:34 +0000

    refactor BasicRuleEvaluator into small functions

--- internal/common/client.go
+++ internal/common/client.go
@@ -35,24 +35,88 @@ func RegisterClient(id shared.ClientID, c Client) {
 	RegisteredClients[id] = c
 }
 
+func createVarList(variables []string) ([]float64, error){
+	var variableVect []float64
+
+    for _, v := range variables {
+        if val, varOk := rules.VariableMap[v]; varOk {
+            variableVect = append(variableVect, val.Values...)
+        } else {
+            return nil, errors.New(fmt.Sprintf("Variable: '%v' not found in global variable cache", v))
+        }
+    }
+
+    variableVect = append(variableVect, 1)
+    return variableVect, nil
+
+}
+
+func ruleMul(variableVect []float64, ApplicableMatrix mat.Dense)(*mat.VecDense){
+    nRows,_ := ApplicableMatrix.Dims()
+
+    variableFormalVect := mat.NewVecDense(len(variableVect), variableVect)
+
+    actual := make([]float64, nRows)
+
+    c := mat.NewVecDense(nRows, actual)
+
+    c.MulVec(&ApplicableMatrix, variableFormalVect)
+
+    return c
+
+}
+
+func genResult(aux mat.VecDense, c *mat.VecDense)([]bool, error){
+    var resultVect []bool
+    nRows, _ := c.Dims()
+
+    for i := 0; i < nRows; i++ {
+        res := false
+        switch interpret := aux.AtVec(i); interpret {
+        case 0:
+            res = c.AtVec(i) == 0
+        case 1:
+            res = c.AtVec(i) > 0
+        case 2:
+            res = c.AtVec(i) >= 0
+        case 3:
+            res = c.AtVec(i) != 0
+        default:
+            return nil, errors.New(fmt.Sprintf("At auxillary vector entry: '%v' aux value outside of 0-3: '%v' was found", i, interpret))
+        }
+        resultVect = append(resultVect, res)
+    }
+
+    return resultVect, nil
+
+}
+
+func checkForFalse(resultVect []bool)(bool){
+
+    var finalBool = true
+
+    for _, v := range resultVect {
+        finalBool = finalBool && v
+    }
+
+    return finalBool
+
+}
+
+
+
+
 // BasicRuleEvaluator implements a basic version of the Matrix rule evaluator, provides single boolean output (and error if present)
 func BasicRuleEvaluator(ruleName string) (bool, error) {
-	var variableVect []float64
+
 	if rm, ok := rules.AvailableRules[ruleName]; ok {
-		variables := rm.RequiredVariables
-
-		for _, v := range variables {
-			if val, varOk := rules.VariableMap[v]; varOk {
-				variableVect = append(variableVect, val.Values...)
-			} else {
-				return false, errors.Errorf("Variable: '%v' not found in global variable cache", v)
-			}
-		}
-
-		variableVect = append(variableVect, 1)
+        variableVect, err := createVarList(rm.RequiredVariables)
+        if err != nil{
+            return false,  err
+        }
 
 		//Checking dimensions line up
-		nRows, nCols := rm.ApplicableMatrix.Dims()
+		_, nCols := rm.ApplicableMatrix.Dims()
 
 		if nCols != len(variableVect) {
 			return false, errors.Errorf(
@@ -63,41 +127,14 @@ func BasicRuleEvaluator(ruleName string) (bool, error) {
 			)
 		}
 
-		variableFormalVect := mat.NewVecDense(len(variableVect), variableVect)
+        c := ruleMul(variableVect, rm.ApplicableMatrix)
 
-		actual := make([]float64, nRows)
+		resultVect, err := genResult(rm.AuxiliaryVector, c)
+        if err != nil{
+            return false,  err
+        }
 
-		c := mat.NewVecDense(nRows, actual)
-
-		c.MulVec(&rm.ApplicableMatrix, variableFormalVect)
-		aux := rm.AuxiliaryVector
-
-		var resultVect []bool
-
-		for i := 0; i < nRows; i++ {
-			res := false
-			switch interpret := aux.AtVec(i); interpret {
-			case 0:
-				res = c.AtVec(i) == 0
-			case 1:
-				res = c.AtVec(i) > 0
-			case 2:
-				res = c.AtVec(i) >= 0
-			case 3:
-				res = c.AtVec(i) != 0
-			default:
-				return false, errors.Errorf("At auxillary vector entry: '%v' aux value outside of 0-3: '%v' was found", i, interpret)
-			}
-			resultVect = append(resultVect, res)
-		}
-
-		var finalBool = true
-
-		for _, v := range resultVect {
-			finalBool = finalBool && v
-		}
-
-		return finalBool, nil
+		return checkForFalse(resultVect), nil
 	} else {
 		return false, errors.Errorf("rule name: '%v' provided doesn't exist in global rule list", ruleName)
 	}
