8f8e87c70f340a48873a6940386eb2122def4c97
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2020-12-13 14:04:08 +0100

    Add error handling to IITO server

--- internal/server/iito.go
+++ internal/server/iito.go
@@ -34,28 +34,43 @@ func (s *SOMASServer) runGiftSession() ([]common.Action, error) {
 		return nil, err
 	}
 	// Process actions
+	for key, value := range giftHistoryDict {
+		s.logf("Gifts from %s: %v\n", key, value)
+	}
 	defer s.logf("finish runGiftSession")
 	return nil, nil
 }
 
 func (s *SOMASServer) getGiftRequests() (shared.GiftDict, error) {
 	giftRequestDict := shared.GiftDict{}
+	var err error
 	for id, client := range s.clientMap {
-		giftRequestDict[id] = client.RequestGift()
+		giftRequestDict[id], err = client.RequestGift()
+		if err != nil {
+			return giftRequestDict, err
+		}
 	}
 	return giftRequestDict, nil
 }
 func (s *SOMASServer) getGiftOffers(giftRequestDict shared.GiftDict) (map[shared.ClientID]shared.GiftDict, error) {
 	giftOfferDict := map[shared.ClientID]shared.GiftDict{}
+	var err error
 	for id, client := range s.clientMap {
-		giftOfferDict[id] = client.OfferGifts(giftRequestDict)
+		giftOfferDict[id], err = client.OfferGifts(giftRequestDict)
+		if err != nil {
+			return giftOfferDict, err
+		}
 	}
 	return giftOfferDict, nil
 }
 func (s *SOMASServer) getGiftAcceptance(giftOffersDict map[shared.ClientID]shared.GiftDict) (map[shared.ClientID]shared.GiftInfoDict, error) {
 	acceptedGifts := map[shared.ClientID]shared.GiftInfoDict{}
+	var err error
 	for id, client := range s.clientMap {
-		acceptedGifts[id] = client.AcceptGifts(giftOffersDict[id])
+		acceptedGifts[id], err = client.AcceptGifts(giftOffersDict[id])
+		if err != nil {
+			return acceptedGifts, err
+		}
 	}
 	return acceptedGifts, nil
 }
@@ -63,7 +78,10 @@ func (s *SOMASServer) getGiftAcceptance(giftOffersDict map[shared.ClientID]share
 func (s *SOMASServer) distributeGiftHistory(acceptedGifts map[shared.ClientID]shared.GiftInfoDict) error {
 	//Process acceptedGifts
 	for id, client := range s.clientMap {
-		client.UpdateGiftInfo(acceptedGifts[id])
+		err := client.UpdateGiftInfo(acceptedGifts[id])
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
