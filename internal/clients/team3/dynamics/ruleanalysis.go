ddcc53150cc6d43aaaca52605b7f58d83cb84d6a
Neelesh99 <Krishna1965!>
2020-12-14 19:43:58 +0000

    Started dynamics package

---
+++ internal/clients/team3/dynamics/ruleanalysis.go
@@ -0,0 +1,115 @@
+package dynamics
+
+import (
+	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
+	"github.com/pkg/errors"
+	"gonum.org/v1/gonum/mat"
+)
+
+func calculateDistanceFromSubSpace(dynamics []dynamic, location mat.VecDense) {
+	var distances []float64
+
+	for _, v := range dynamics {
+		distances = append(distances, findDistanceToHyperplane(v.w, v.b, location))
+	}
+
+}
+
+func calculateIntersection(constraint dynamic, location mat.VecDense) {
+
+}
+
+// buildSelectedDynamics depending on list of indexes, this function will build dynamics
+func buildSelectedDynamics(matrixOfRules rules.RuleMatrix, auxiliaryVector mat.VecDense, selectedRules []int) []dynamic {
+	matrixVal := matrixOfRules.ApplicableMatrix
+	nRows, _ := matrixVal.Dims()
+	var returnDynamics []dynamic
+	for i := 0; i < nRows; i++ {
+		if findInSlice(selectedRules, i) {
+			tempWeights := mat.VecDenseCopyOf(matrixVal.RowView(i))
+			returnDynamics = append(returnDynamics, constructSingleDynamic(*tempWeights, auxiliaryVector.AtVec(i)))
+		}
+	}
+	return returnDynamics
+}
+
+// buildAllDynamics takes an entire rule matrix and returns all dynamics from it
+func buildAllDynamics(matrixOfRules rules.RuleMatrix, auxiliaryVector mat.VecDense) []dynamic {
+	matrixVal := matrixOfRules.ApplicableMatrix
+	nRows, _ := matrixVal.Dims()
+	var returnDynamics []dynamic
+	for i := 0; i < nRows; i++ {
+		tempWeights := mat.VecDenseCopyOf(matrixVal.RowView(i))
+		returnDynamics = append(returnDynamics, constructSingleDynamic(*tempWeights, auxiliaryVector.AtVec(i)))
+	}
+	return returnDynamics
+}
+
+// constructSingleDynamic builds a dynamic from a row of the rule matrix
+func constructSingleDynamic(ruleRow mat.VecDense, auxCode float64) dynamic {
+	nRows, _ := ruleRow.Dims()
+
+	offset := ruleRow.AtVec(nRows - 1)
+
+	weights := mat.VecDenseCopyOf(ruleRow.SliceVec(0, nRows-1))
+
+	return createDynamic(*weights, offset, auxCode)
+
+}
+
+// findMinimumRequirements takes in addresses of deficient variables and calculates the minimum values required at those entries to satisfy the rule
+func findMinimumRequirements(deficients []int, aux mat.VecDense) []float64 {
+	var outputMins []float64
+	for _, v := range deficients {
+		switch interpret := aux.AtVec(v); interpret {
+		case 0:
+			outputMins = append(outputMins, 0)
+		case 1:
+			outputMins = append(outputMins, 1)
+		case 2:
+			outputMins = append(outputMins, 0)
+		case 3:
+			outputMins = append(outputMins, 1)
+		default:
+			outputMins = append(outputMins, 0)
+		}
+	}
+	return outputMins
+}
+
+// identifyDeficiencies checks result of rule evaluation and finds entries of result vector that do not comply
+func identifyDeficiencies(b mat.VecDense, aux mat.VecDense) ([]int, error) {
+	if checkDimensions(b, aux) {
+		nRows, _ := b.Dims()
+		var outputData []int
+		for i := 0; i < nRows; i++ {
+			if aux.AtVec(i) > 4 || aux.AtVec(i) < 0 {
+				return []int{}, errors.Errorf("Auxilliary vector at entry '%v' has aux code out of range: '%v'", i, aux.AtVec(i))
+			}
+			if !satisfy(b.AtVec(i), aux.AtVec(i)) {
+				outputData = append(outputData, i)
+			}
+		}
+		return outputData, nil
+	} else {
+		return []int{}, errors.Errorf("Vectors '%v' and '%v' do not have the same dimensions", b, aux)
+	}
+}
+
+// satisfy checks whether a condition is met based on result vector value and auxiliary code
+func satisfy(x float64, a float64) bool {
+	switch a {
+	case 0:
+		return x == 0
+	case 1:
+		return x > 0
+	case 2:
+		return x >= 0
+	case 3:
+		return x != 0
+	case 4:
+		return true
+	default:
+		return false
+	}
+}
