ab3160813606c43119087bd81e36a9604ca99ae2
Neelesh99 <Krishna1965!>
2020-12-15 17:21:25 +0000

    Added Rules in play list for IIGO manipulation

--- internal/common/rules/globalrulescache.go
+++ internal/common/rules/globalrulescache.go
@@ -7,6 +7,8 @@ import (
 
 var AvailableRules = map[string]RuleMatrix{}
 
+var RulesInPlay = map[string]RuleMatrix{}
+
 // RegisterNewRule Creates and registers new rule based on inputs
 func RegisterNewRule(ruleName string, requiredVariables []string, applicableMatrix mat.Dense, auxiliaryVector mat.VecDense) (*RuleMatrix, error) {
 	if _, ok := AvailableRules[ruleName]; ok {
@@ -16,4 +18,25 @@ func RegisterNewRule(ruleName string, requiredVariables []string, applicableMatr
 	rm := RuleMatrix{ruleName: ruleName, RequiredVariables: requiredVariables, ApplicableMatrix: applicableMatrix, AuxiliaryVector: auxiliaryVector}
 	AvailableRules[ruleName] = rm
 	return &rm, nil
+}
+
+func PullRuleIntoPlay(rulename string) error {
+	if _, ok := AvailableRules[rulename]; ok {
+		if _, ok := RulesInPlay[rulename]; ok {
+			return errors.Errorf("Rule '%v' is already in play", rulename)
+		}
+		RulesInPlay[rulename] = AvailableRules[rulename]
+		return nil
+	} else {
+		return errors.Errorf("Rule '%v' is not available in rules cache", rulename)
+	}
+}
+
+func PullRuleOutOfPlay(rulename string) error {
+	if _, ok := RulesInPlay[rulename]; ok {
+		delete(RulesInPlay, rulename)
+		return nil
+	} else {
+		return errors.Errorf("Rule '%v' is already in play", rulename)
+	}
 }
