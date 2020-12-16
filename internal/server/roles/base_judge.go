9ba99e0daa650010bd3f105ca55e86fa6053dfc1
nidhijaju <nidhijaju127@gmail.com>
2020-12-16 22:40:58 +0900

    fix var declarations

--- internal/server/roles/base_judge.go
+++ internal/server/roles/base_judge.go
@@ -5,10 +5,10 @@ import (
 )
 
 type BaseJudge struct {
-	int id
-	int budget
-	int president_salary
-	int ballotID
-	int resAlocID
-	map actionLog
+	id int
+    budget int
+	president_salary int
+	ballotID int
+	resAlocID int
+	actionLog map
 }
