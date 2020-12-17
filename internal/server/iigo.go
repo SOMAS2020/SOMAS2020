c05ed6af5f1034548ffe0c8bc708b901f8e3d62c
lhl2617 <33488131+lhl2617@users.noreply.github.com>
2020-12-17 21:20:02 +0000

    [INFRA/CORE] Dispatch actions immediately + add mid-turn game state update (#54)

--- internal/server/iigo.go
+++ internal/server/iigo.go
@@ -1,11 +1,9 @@
 package server
 
-import "github.com/SOMAS2020/SOMAS2020/internal/common"
-
 // runIIGO : IIGO decides rule changes, elections, sanctions
-func (s *SOMASServer) runIIGO() ([]common.Action, error) {
+func (s *SOMASServer) runIIGO() error {
 	s.logf("start runIIGO")
 	defer s.logf("finish runIIGO")
 	// TODO:- IIGO team
-	return nil, nil
+	return nil
 }
--- internal/server/iito.go
+++ internal/server/iito.go
@@ -1,12 +1,10 @@
 package server
 
-import "github.com/SOMAS2020/SOMAS2020/internal/common"
-
 // runIITO : IITO makes recommendations about the optimal (and fairest) contributions this term
 // to mitigate the common pool dilemma
-func (s *SOMASServer) runIITO() ([]common.Action, error) {
+func (s *SOMASServer) runIITO() error {
 	s.logf("start runIITO")
 	defer s.logf("finish runIITO")
 	// TODO:- IITO team
-	return nil, nil
+	return nil
 }
