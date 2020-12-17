66a6c72264d75e7650f5ca29382ed5e90dc2243f
Preet Lalli <preetlalli@gmail.com>
2020-12-17 09:49:28 +0000

    added speaker base variables and function stubs

--- internal/server/roles/base_speaker.go
+++ internal/server/roles/base_speaker.go
@@ -1,9 +1,24 @@
 package roles
 
-import (
-
-)
-
 type BaseSpeaker struct {
-	
+	id					int
+	budget				int
+	judgeSalary 		int
+	ruleToVote			int
+}
+
+func (s *BaseSpeaker) WithdrawJudgeSalary() {
+
+}
+
+func (s *BaseSpeaker) PayJudge() {
+
+}
+
+func (s *BaseSpeaker) RunVote() {
+
+}
+
+func (s *BaseSpeaker) UpdateRules() {
+
 }
