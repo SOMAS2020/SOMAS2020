8aea1ec73a0b430f21f4e38042c657d85c670cf5
Noe Guisset <noe.guisset@gmail.com>
2020-12-18 16:58:52 +0000

    creation of base methods

--- internal/server/roles/base_president.go
+++ internal/server/roles/base_president.go
@@ -1,7 +1,7 @@
 package roles
 
 //base President Object
-type basePresident struct {
+type BasePresident struct {
 	id                 int
 	budget             int
 	speakerSalary      int
@@ -9,4 +9,29 @@ type basePresident struct {
 	resourceAllocation map[int]int
 	ruleToVote         int
 	taxAmount          int
+}
+
+// Set taxation amount for all of the living islands
+func (p *BasePresident) setTaxationAmount() {
+
+}
+
+// Obtain a rule proposal from each of the living islands
+func (p *BasePresident) getRuleProposals() {
+
+}
+
+// Broadcast tax amount to each of the living islands
+func (p *BasePresident) signalTaxationAmount() {
+
+}
+
+// Broadcast approved common pool resource request to each of the living islands
+func (p *BasePresident) signalAllocationRequest() {
+
+}
+
+// Send rule to be voted on to Speaker
+func (p *BasePresident) sendRuleToSpeaker() {
+
 }
