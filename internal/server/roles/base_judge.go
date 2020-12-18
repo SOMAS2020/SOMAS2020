1f88c3684cb5797eb07cf60abad8308444512c9e
nidhijaju <nidhijaju127@gmail.com>
2020-12-18 18:07:23 +0900

    add some more

--- internal/server/roles/base_judge.go
+++ internal/server/roles/base_judge.go
@@ -1,14 +1,44 @@
 package roles
 
-import (
-
-)
-
+// base Judge object
 type BaseJudge struct {
-	id int
-    budget int
-	president_salary int
+	id 	   int
+	budget int
+	presidentSalary int
 	ballotID int
 	resAlocID int
-	actionLog map
+	actionLog map[int]string // not sure about this currently
+}
+
+
+func (j *BaseJudge) withdrawPresidentSalary() {
+	// Withdraw president salary from the common pool
+}
+
+func (j *BaseJudge) payPresident() {
+	// Pay the president
+}
+
+func (j *BaseJudge) inspectBallot() {
+	// 1. Evaluate difference between newRules and oldRules to check
+	//    rule changes are in line with ruleToVote in previous ballot
+	// 2. Compare each ballot action adheres to rules in ruleSet matrix
+}
+
+func (j *BaseJudge) inspectAllocation() {
+	// 1. Evaluate difference between commonPoolNew and commonPoolOld
+	//    to check resource allocation changes are in line with resourceRequests
+	//    in previous resourceAllocation
+	// 2. Compare each resource allocation action adheres to rules in ruleSet
+	//    matrix
+}
+
+func (j *BaseJudge) declareSpeakerPerformance() {
+	// result := "pass/fail" based on outcome of audit in inspectBallot()
+	// Broadcast (j.ballotID, result, "Speaker") to all islands
+}
+
+func (j *BaseJudge) declarePresidentPerformance() {
+	// result := "pass/fail" based on outcome of audit in inspectAllocation()
+	// Broadcast (j.resAlocID, result, "President") to all islands
 }
--- internal/server/roles/judge.go
+++ internal/server/roles/judge.go
@@ -1,14 +1,12 @@
 package roles
 
-import (
-    "fmt"
-)
-
-type Judge Interface {
-	withdrawPresidentSalary()
-	payPresident()
-	inspectBallot()
-	inspectAllocation()
-	declareSpeakerPerformance()
-	declarePresidentPerformance()
+// Judge is an interface that is implemented by BaseJudge but can also be
+// optionally implemented by individual islands.
+type Judge interface {
+	withdrawPresidentSalary() error
+	payPresident() error
+	inspectBallot() error
+	inspectAllocation() error
+	declareSpeakerPerformance() error
+	declarePresidentPerformance() error
 }
