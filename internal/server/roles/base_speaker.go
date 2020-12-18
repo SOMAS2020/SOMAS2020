7f8defb107da9facebc8ec77ee79af8a3c208241
Spruudis <r.spuris@gmail.com>
2020-12-18 21:22:04 +0000

    SetRuleToVote & Very Black-boxy RunVote

--- internal/server/roles/base_speaker.go
+++ internal/server/roles/base_speaker.go
@@ -15,8 +15,53 @@ func (s *BaseSpeaker) PayJudge() {
 
 }
 
-func (s *BaseSpeaker) RunVote() {
+// Receive a rule to call a vote on
+func (s *BaseSpeaker) SetRuleToVote(r int) {
+	s.ruleToVote = r
+}
 
+func (s *BaseSpeaker) RunVote() error {
+
+	if s.ruleToVote == -1 {
+		// No rules were proposed by the islands
+		return nil
+	} else{
+		//Run the vote
+		//Creating the object = opening the ballot?
+		v := voting.Vote{s.ruleToVote}
+		//TODO: updateTurnHistory of rule given to vote on vs ruleToVote
+
+		//Providing optional array of islandID-s = closing ballot early/
+		//controlling which islands are allowed to vote if some are not permitted?
+		//Providing optional string of result counting method
+
+		//Receive result, number of islands alive, number of islands that voted
+		votes, nia, niv := voting.CallVote(s.id)
+		//Speaker Id passed in for robust logging
+
+		//Log result TODO: Can this log be moved to inside voting?
+		noIslandAlive := rules.VariableValuePair{
+			VariableName: "no_islands_alive",
+			Values:       []float64{nia},
+		}
+		noIslandsVoting := rules.VariableValuePair{
+			VariableName: "no_islands_voted",
+			Values:       []float64{niv},
+		}
+
+		err := updateTurnHistory(s.id, []rules.VariableValuePair{noIslandAlive, noIslandsVoting})
+
+		if err != nil {
+			return err
+		} else {
+			//Count the votes
+			result := voting.CountVotes(votes, "majority")
+
+			//Declare the result = UpdateRules?
+			s.UpdateRules(s.ruleToVote, result)
+		}
+
+	}
 }
 
 func (s *BaseSpeaker) UpdateRules() {
