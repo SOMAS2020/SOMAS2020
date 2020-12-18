29a97581b2b6f318d01fc0150dde1cf1e0139fde
Neelesh99 <Krishna1965!>
2020-12-18 14:21:46 +0000

    Added History (Action Log) and accoutnability structs as well as skeleton for team6

--- internal/server/roles/orchestration.go
+++ internal/server/roles/orchestration.go
@@ -1,1 +1,22 @@
 package roles
+
+import "github.com/SOMAS2020/SOMAS2020/internal/common/rules"
+
+func callVote(speakerID int) {
+	// Do voting
+
+	noIslandAlive := rules.VariableValuePair{
+		VariableName: "no_islands_alive",
+		Values:       []float64{5},
+	}
+	noIslandsVoting := rules.VariableValuePair{
+		VariableName: "no_islands_voted",
+		Values:       []float64{5},
+	}
+	err := updateTurnHistory(speakerID, []rules.VariableValuePair{noIslandAlive, noIslandsVoting})
+	if err != nil {
+		// exit with error
+	} else {
+		// carry on
+	}
+}
