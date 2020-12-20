86b5bf19336768fd2aeb3880e702deae1665d0c7
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-12-20 20:22:03 +0100

    Cleaned up president tests

--- internal/server/roles/base_president_test.go
+++ internal/server/roles/base_president_test.go
@@ -8,11 +8,6 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
-// Failing test
-func TestPresident(t *testing.T) {
-	t.Error()
-}
-
 func mapSum(in_map map[int]int) int {
 	sum := 0
 	for _, val := range in_map {
@@ -34,7 +29,7 @@ func TestAllocationRequests(t *testing.T) {
 		want  error
 	}{
 		{
-			name:  "Too many resources",
+			name:  "Limitied resources",
 			input: allocInput{resourceRequests: map[int]int{1: 5, 2: 10, 3: 15, 4: 20, 5: 25, 6: 30}, commonPoolResource: 100},
 			reply: map[int]int{1: 3, 2: 7, 3: 10, 4: 14, 5: 17, 6: 21},
 			want:  nil,
@@ -45,40 +40,28 @@ func TestAllocationRequests(t *testing.T) {
 			reply: map[int]int{1: 5, 2: 10, 3: 15, 4: 20, 5: 25, 6: 30},
 			want:  nil,
 		},
+		{
+			name:  "No resources",
+			input: allocInput{resourceRequests: map[int]int{1: 5, 2: 10, 3: 15, 4: 20, 5: 25, 6: 30}, commonPoolResource: 0},
+			reply: map[int]int{1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0},
+			want:  nil,
+		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			// mClient := &mockClientEcho{
-			// 	id:   shared.Team1,
-			// 	echo: tc.reply,
-			// }
-			// server := &SOMASServer{
-			// 	clientMap: map[shared.ClientID]baseclient.Client{
-			// 		shared.Team1: mClient,
-			// 	},
-			// }
 
 			president := &basePresident{
 				id:     int(shared.Team1),
 				budget: 50,
-				//resourceRequests:
 			}
 
-			val, _ := president.evaluateAllocationRequests(tc.input.resourceRequests, tc.input.commonPoolResource)
-			if tc.want == nil {
+			val, err := president.evaluateAllocationRequests(tc.input.resourceRequests, tc.input.commonPoolResource)
+			if err == nil && tc.want == nil {
 				if !reflect.DeepEqual(tc.reply, val) {
 					t.Errorf("%v - Failed. Expected %v, got %v", tc.name, tc.reply, val)
-
 				}
-
 			}
-			//testutils.CompareTestErrors(tc.want, got, t)
-			// president.resourceAllocation
-
-			// Not sure how to test considering this class can be overridable?
-			// Could check the sum is less than the given common pool
-			// testutils.CompareTestErrors(tc.want, got, t)
 		})
 	}
 }
