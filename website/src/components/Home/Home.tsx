2063d0eb7641cd97bd15d385772ba4b9f892de9b
lhl2617 <lhlee888@hotmail.com>
2020-12-20 18:54:22 +0000

    [INFRA/CORE] Basic website structure & raw output

---
+++ website/src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../assets/logo/logo512.png';
+import styles from './Home.module.css';
+
+function App() {
+  return (
+    <div className={styles.root}>
+      <img src={logo} className={styles.appLogo} alt="logo" />
+      <p className={styles.text}>
+        Self-Organising Multi-Agent Systems 2020
+        </p>
+    </div>
+  );
+}
+
+export default App;
