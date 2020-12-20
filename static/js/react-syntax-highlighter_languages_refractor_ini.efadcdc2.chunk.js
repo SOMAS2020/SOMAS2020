10ba1cfab5dc30d4c055d290dda8c88bff900c0e
lhl2617 <lhlee888@hotmail.com>
2020-12-20 18:53:20 +0000

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.efadcdc2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[60],{183:function(t,s,e){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.efadcdc2.chunk.js.map
