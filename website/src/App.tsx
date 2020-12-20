ae349a58afda87c2e9a1a424f72782d68363e889
lhl2617 <33488131+lhl2617@users.noreply.github.com>
2020-12-20 16:43:45 +0000

    [INFRA/CORE] Add repo VCS info into output.txt (#74) (#75)

---
+++ website/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './assets/logo/logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Self-Organising Multi-Agent Systems 2020
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
