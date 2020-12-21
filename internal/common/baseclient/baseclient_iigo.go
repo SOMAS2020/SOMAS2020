2720b6db211c291896fca730e8884cfca6d75ec6
Preet Lalli <preetlalli@gmail.com>
2020-12-21 10:59:12 +0000

    added imports for ruleproposal func

--- internal/common/baseclient/baseclient_iigo.go
+++ internal/common/baseclient/baseclient_iigo.go
@@ -1,18 +1,28 @@
 package baseclient
 
+import (
+	"math/rand"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
+)
+
 // CommonPoolResourceRequest is called by the President in IIGO to
 // request an allocation of resources from the common pool.
-func(c *BaseClient) CommonPoolResourceRequest()int{
-
+func(c *BaseClient) CommonPoolResourceRequest() int{
+	//TODO : Team 5 need access to the common pool gamestate...
+	return 0
 }
 
 // ResourceReport is an island's self-report of its own resources.
-func(c *BaseClient) ResourceReport()int{
+func(c *BaseClient) ResourceReport() int{
 	return c.clientGameState.ClientInfo.Resources
 }
 
 // RuleProposal is called by the President in IIGO to propose a
 // rule to be voted on.
-func(c *BaseClient) RuleProposal()string{
-
+func(c *BaseClient) RuleProposal() string{
+	allrules := make([]string, 0, len(rules.RulesInPlay))
+    for r := range rules.RulesInPlay {
+        allrules = append(allrules, r)
+	}
+	return allrules[rand.Intn(len(allrules))]
 }
