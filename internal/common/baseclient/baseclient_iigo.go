82d405edb652ad1f79c0348d55a9391033522a81
Preet Lalli <preetlalli@gmail.com>
2020-12-21 10:20:06 +0000

    stubbing baseclient functions for IIGO

---
+++ internal/common/baseclient/baseclient_iigo.go
@@ -0,0 +1,13 @@
+package baseclient
+
+func(c *BaseClient) getCommonPoolResourceRequest(){
+
+}
+
+func(c *BaseClient) getResourceReport()int{
+	return c.clientGameState.ClientInfo.Resources
+}
+
+func(c *BaseClient) getRuleProposal(){
+
+}
