4db19a8705857d0185a3dc9facfe6f94f5cb2751
Neelesh99 <Krishna1965!>
2020-12-21 10:53:51 +0000

    Added implementations for some of the baseclient_iigo.go methods

--- internal/common/baseclient/baseclient_iigo.go
+++ internal/common/baseclient/baseclient_iigo.go
@@ -1,18 +1,24 @@
 package baseclient
 
+import "github.com/SOMAS2020/SOMAS2020/internal/common/rules"
+
 // CommonPoolResourceRequest is called by the President in IIGO to
 // request an allocation of resources from the common pool.
-func(c *BaseClient) CommonPoolResourceRequest()int{
-
+func (c *BaseClient) CommonPoolResourceRequest() int {
+	return 20
 }
 
 // ResourceReport is an island's self-report of its own resources.
-func(c *BaseClient) ResourceReport()int{
+func (c *BaseClient) ResourceReport() int {
 	return c.clientGameState.ClientInfo.Resources
 }
 
 // RuleProposal is called by the President in IIGO to propose a
 // rule to be voted on.
-func(c *BaseClient) RuleProposal()string{
-
+func (c *BaseClient) RuleProposal() string {
+	allRules := rules.AvailableRules
+	for k, _ := range allRules {
+		return k
+	}
+	return ""
 }
