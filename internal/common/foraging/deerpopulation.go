1233a41176f8a3dcc621dd4919e57a2bc1c8a6c8
James Teversham <jamestevers@gmail.com>
2020-12-22 22:21:41 +0200

    Updated DeerPopulation simulation

--- internal/common/foraging/deerpopulation.go
+++ internal/common/foraging/deerpopulation.go
@@ -19,7 +19,7 @@ type DeerPopulationModel struct {
 // it easier to read logs. DO NOT use other loggers that will mess logs up!
 // BASE: Do not overwrite in team client.
 func (dp *DeerPopulationModel) Logf(format string, a ...interface{}) {
-	log.Printf("DeerPop [t=%.1f]: %v", dp.T, fmt.Sprintf(format, a...))
+	log.Printf("[SERVER]: DeerPop [t=%.1f]: %v", dp.T, fmt.Sprintf(format, a...))
 }
 
 // CreateBasicDeerPopulationModel returns a basic population model based on dP/dt = k(N-y) model. k = growth coeff., N = max deer (constants).
@@ -41,12 +41,12 @@ func createBasicDeerPopulationModel() DeerPopulationModel {
 // Note: if only simulating for one turn ('step'), len(deerConsumption) = 1
 func (dp *DeerPopulationModel) Simulate(deerConsumption []int) {
 	deStep := dp.deProblem.StepDeltaY()
-	t := dp.T
-	y := dp.Population
+
 	for i := 0; i < len(deerConsumption); i++ { // note: can use DE.SolveUntilT(10) but in this case we want access to y, t at each iteration
 		y0 := dp.Population - float64(deerConsumption[i])
-		t, y = deStep(float64(-deerConsumption[i])) // this will update population, t in receiver
+		t, y := deStep(float64(-deerConsumption[i])) // this will update population, t in receiver
 		dp.Logf("P(t): %.2f, \tdeer after hunt %v, \tdeer end of day: %v, \tdeer hunted: %v\n", y, int(y0), int(y), deerConsumption[i])
+
+		dp.Population, dp.T = y, t
 	}
-	dp.Population, dp.T = y, t
 }
