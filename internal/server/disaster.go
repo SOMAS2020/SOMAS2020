7ecfc8a272ae78756ea86f51e89da240c151ba6d
lhl2617 <lhlee888@hotmail.com>
2020-12-22 00:05:42 +0000

    [INFRA/CORE] Adjust linter

--- internal/server/disaster.go
+++ internal/server/disaster.go
@@ -1,9 +1,15 @@
 package server
 
+import "fmt"
+
 // probeDisaster checks if a disaster occurs this turn
 func (s *SOMASServer) probeDisaster() (bool, error) {
 	s.logf("start probeDisaster")
 	defer s.logf("finish probeDisaster")
 	// TODO:- env team
 	return false, nil
+}
+
+func ServerReee() {
+	fmt.Println("REE")
 }
