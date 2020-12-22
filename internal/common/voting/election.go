c1b016112dd25ac80be887eb68d1ae8fe2f26cef
Preet Lalli <preetlalli@gmail.com>
2020-12-22 21:27:11 +0000

    adding voting methods

--- internal/common/voting/election.go
+++ internal/common/voting/election.go
@@ -6,14 +6,24 @@ import (
 )
 
 type Election struct {
-	roleToElect	baseclient.Role
+	roleToElect		baseclient.Role
+	votingMethod	ElectionVotingMethod
 	islandsToVote []shared.ClientID
 	votes         [][]shared.ClientID
 }
 
+type ElectionVotingMethod = int
+
+const (
+	BordaCount
+	Plurality
+	Majority
+)
+
 // ProposeMotion sets the role to be voted on
-func (e *Election) ProposeElection(role baseclient.Role) {
-	roleToElect = role
+func (e *Election) ProposeElection(role baseclient.Role, method ElectionVotingMethod) {
+	e.roleToElect = role
+	e.votingMethod = method
 }
 
 // OpenBallot sets the islands eligible to vote.
@@ -24,11 +34,32 @@ func (e *Election) OpenBallot(clientIDs []shared.ClientID) {
 // Vote gets votes from eligible islands.
 func (e *Election) Vote(clientMap map[shared.ClientID]baseclient.Client) {
 	for _, island := range v.islandsToVote {
-		v.votes = append(v.votes, clientMap[island].GetVoteForElection(v.roleToElect))
+		e.votes = append(v.votes, clientMap[island].GetVoteForElection(v.roleToElect))
 	}
 }
 
 // CloseBallot counts the votes received and returns the result.
 func (e *Election) CloseBallot() shared.ClientID {
+
+	switch e.votingMethod {
+	case BordaCount :
+		result = bordaCountResult()
+	case Plurality :
+		result = pluralityResult()
+	case Majority :
+		result = majorityResult()
+	}
+	return result
+}
+
+func (e* Election) bordaCountResult() shared.ClientID {
+
+}
+
+func (e* Election) pluralityResult() shared.ClientID {
+	
+}
+
+func (e* Election) majorityResult() shared.ClientID {
 	
 }
