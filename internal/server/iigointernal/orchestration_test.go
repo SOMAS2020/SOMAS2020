fa531e75250da39478f070254887856f3c43adb1
Neelesh99 <Krishna1965!>
2020-12-23 22:46:52 +0000

    Regression testing added

---
+++ internal/server/iigointernal/orchestration_test.go
@@ -0,0 +1,27 @@
+package iigointernal
+
+import (
+	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/gamestate"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
+	"testing"
+)
+
+func TestPutSalaryBack(t *testing.T) {
+	fakeGameState := gamestate.GameState{
+		CommonPool: 149,
+		IIGOInfo: gamestate.IIGOBaseRoles{
+			BasePresident: &featurePresident,
+			BaseSpeaker:   &featureSpeaker,
+			BaseJudge:     &featureJudge,
+		},
+	}
+	err := RunIIGO(&fakeGameState, &map[shared.ClientID]baseclient.Client{})
+	if err == nil {
+		t.Errorf("IIGO didn't throw error when salaries couldn't be paid")
+	} else {
+		if fakeGameState.CommonPool != 149 {
+			t.Errorf("Common pool contained '%v' expected '%v'", fakeGameState.CommonPool, 149)
+		}
+	}
+}
