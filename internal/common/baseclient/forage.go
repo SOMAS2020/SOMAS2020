fbc181a6ac770aedddb75233cf198684ef6b9c07
Michalis Pardalos <m.pardalos@gmail.com>
2020-12-24 03:16:13 +0200

    Reduce foraging contribution for BaseClient

--- internal/common/baseclient/forage.go
+++ internal/common/baseclient/forage.go
@@ -9,5 +9,8 @@ import (
 // DecideForage makes a foraging decision, currently you can only forage deer, but fishing will be available later
 // the forageContribution can not be larger than the total resources available
 func (c *BaseClient) DecideForage() (shared.ForageDecision, error) {
-	return shared.ForageDecision{Type: shared.DeerForageType, Contribution: shared.Resources(rand.Float64() * 100)}, nil
+	return shared.ForageDecision{
+		Type: shared.DeerForageType,
+		Contribution: shared.Resources(rand.Float64() * 5),
+	}, nil
 }
