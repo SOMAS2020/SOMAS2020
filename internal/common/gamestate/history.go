e698112e395a4f62bd31b7fe0160b6645f891062
Neelesh99 <Krishna1965!>
2020-12-26 13:02:21 +0000

    Accountability struct moved to shared

--- internal/common/gamestate/history.go
+++ internal/common/gamestate/history.go
@@ -5,15 +5,10 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
-type Accountability struct {
-	ClientID shared.ClientID
-	Pairs    []rules.VariableValuePair
-}
-
-var TurnHistory []Accountability
+var TurnHistory []shared.Accountability
 
 func UpdateTurnHistory(clientID shared.ClientID, pairs []rules.VariableValuePair) error {
-	TurnHistory = append(TurnHistory, Accountability{
+	TurnHistory = append(TurnHistory, shared.Accountability{
 		ClientID: clientID,
 		Pairs:    pairs,
 	})
@@ -21,5 +16,5 @@ func UpdateTurnHistory(clientID shared.ClientID, pairs []rules.VariableValuePair
 }
 
 func clearHistory() {
-	TurnHistory = []Accountability{}
+	TurnHistory = []shared.Accountability{}
 }
