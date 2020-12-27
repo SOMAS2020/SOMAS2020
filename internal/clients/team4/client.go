dbe787c07091e19f3446f5ff08e0c1cf1e014fbc
zx516 <zx516@imperial.ac.uk>
2020-12-27 18:54:27 +0000

    base client function overloading works

--- internal/clients/team4/client.go
+++ internal/clients/team4/client.go
@@ -2,6 +2,9 @@
 package team4
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
@@ -14,4 +17,15 @@ func init() {
 
 type client struct {
 	baseclient.Client
+	yes string //this field is just for testing
+}
+
+func (c *client) StartOfTurn() {
+	c.yes = "for you"
+	c.Logf(`what are you doing?
+	=========================================
+	==============================================
+	================================================`)
+	c.Logf("this is yes " + c.yes)
+	fmt.Println(reflect.TypeOf(c))
 }
