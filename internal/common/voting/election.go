a94ad3ad2219ec3cdb78e3fa7e10180d98b5b10a
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2020-12-28 23:30:12 +0000

    Add files via upload

--- internal/common/voting/election.go
+++ internal/common/voting/election.go
@@ -1,8 +1,11 @@
 package voting
 
 import (
+	"fmt"
+
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
+	"github.com/SOMAS2020/SOMAS2020/pkg/miscutils"
 )
 
 type Election struct {
@@ -12,13 +15,41 @@ type Election struct {
 	votes         [][]shared.ClientID
 }
 
-type ElectionVotingMethod = int
+// ElectionVotingMethod provides enumerated type for selection of voting system to be used
+type ElectionVotingMethod int
 
 const (
 	BordaCount = iota
 	Plurality
 	Majority
 )
+
+func (e ElectionVotingMethod) String() string {
+	strs := [...]string{
+		"BordaCount",
+		"Plurality",
+		"Majority",
+	}
+	if e >= 0 && int(e) < len(strs) {
+		return strs[e]
+	}
+	return fmt.Sprintf("UNKNOWN ElectionVotingMethod '%v'", int(e))
+}
+
+// GoString implements GoStringer
+func (e ElectionVotingMethod) GoString() string {
+	return e.String()
+}
+
+// MarshalText implements TextMarshaler
+func (e ElectionVotingMethod) MarshalText() ([]byte, error) {
+	return miscutils.MarshalTextForString(e.String())
+}
+
+// MarshalJSON implements RawMessage
+func (e ElectionVotingMethod) MarshalJSON() ([]byte, error) {
+	return miscutils.MarshalJSONForString(e.String())
+}
 
 // ProposeMotion sets the role to be voted on
 func (e *Election) ProposeElection(role baseclient.Role, method ElectionVotingMethod) {
