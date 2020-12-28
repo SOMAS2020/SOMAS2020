17e5884884ec74d9ac55eabc93f87b4aa4ad94c6
lhl2617 <lhlee888@hotmail.com>
2020-12-28 20:54:21 +0000

    Add initial go files

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { HashRouter } from "react-router-dom";
 import AppLayout from './containers/AppLayout/AppLayout';
 
 function App() {
+  useEffect(() => {
+
+  })
+
+  const load = async () => {
+    const { instance, module } = await WebAssembly.instantiateStreaming(
+      fetch(`main.wasm`), 
+      window.go.importObject
+    )
+  }
+
   return (
     <HashRouter>
       <AppLayout />
