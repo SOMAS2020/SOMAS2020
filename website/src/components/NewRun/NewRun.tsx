9cdb0f19c68de5759be99ba4bf006e28aed556df
lhl2617 <lhlee888@hotmail.com>
2020-12-29 14:56:18 +0000

    flags working

--- website/src/components/NewRun/NewRun.tsx
+++ website/src/components/NewRun/NewRun.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { CodeBlock } from 'react-code-blocks'
 import { runGame, getFlagsFormats, RunGameReturnType, GoFlag } from "../../wasmAPI"
-import { Alert, Button, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { Alert, Button, Row, Col, OverlayTrigger, Tooltip, Form } from 'react-bootstrap'
 import { useLoadingState, initialLoadingState } from "../../contexts/loadingState"
 
 import styles from '../CIOutput/CIOutput.module.css'
@@ -9,24 +9,35 @@ import styles from '../CIOutput/CIOutput.module.css'
 type Flag = GoFlag & { Value: string }
 
 type flagFormProps = {
-  flag: GoFlag,
+  flag: Flag,
   setFlag: (val: string) => Promise<void>,
+  disabled: boolean,
 }
 
 const FlagForm = (props: flagFormProps) => {
-  const { flag, setFlag } = props
+  const { flag, setFlag, disabled } = props
 
+  const handleChange = async (event: React.ChangeEvent<any>) => {
+    await setFlag(event.target.value)
+  }
   return <Col xs={4}>
-    <OverlayTrigger
-      placement="top"
-      overlay={
-        <Tooltip id={flag.Name}>
-          {flag.Usage}
-        </Tooltip>
-      }
-    >
-      <p style={{ fontWeight: 600 }}>{flag.Name}</p>
-    </OverlayTrigger >
+    <Form>
+      <Form.Group>
+        <Form.Label>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id={flag.Name}>
+                {flag.Usage}
+              </Tooltip>
+            }
+          >
+            <span>{flag.Name}</span>
+          </OverlayTrigger >
+        </Form.Label>
+        <Form.Control value={flag.Value} onChange={handleChange} readOnly={disabled} />
+      </Form.Group>
+    </Form>
   </Col >
 }
 
@@ -34,7 +45,7 @@ const NewRun = () => {
   const [_, setLoading] = useLoadingState()
   const [output, setOutput] = useState<RunGameReturnType | undefined>(undefined)
   const [runError, setRunError] = useState<string | undefined>(undefined)
-  const [flags, setFlags] = useState<Record<string, Flag> | undefined>(undefined)
+  const [flags, setFlags] = useState<Map<string, Flag> | undefined>(undefined)
 
   useEffect(() => {
     loadFlags()
@@ -43,9 +54,9 @@ const NewRun = () => {
   const loadFlags = async () => {
     setLoading({ loading: true, loadingText: `We're finding the archipelago...` })
     try {
-      const goflags = await getFlagsFormats()
-      let fs: Record<string, Flag> = {}
-      goflags.forEach(f => { fs[f.Name] = { ...f, Value: f.DefValue } })
+      const goFlags = await getFlagsFormats()
+      let fs: Map<string, Flag> = new Map()
+      goFlags.forEach(f => { fs.set(f.Name, { ...f, Value: f.DefValue }) })
       setFlags(fs)
     }
     catch (err) {
@@ -56,22 +67,29 @@ const NewRun = () => {
 
   const setFlag = async (flagName: string, val: string) => {
     if (!flags) {
-      throw new Error(`flags undefined!`)
+      // should not happen
+      throw new Error(`Flags not loaded`)
     }
-    if (!(flagName in flags)) {
+    const currFlag = flags.get(flagName)
+
+    if (!currFlag) {
       throw new Error(`Unknown flag name ${flagName}`)
     }
-    const newFlags = flags
-    newFlags[flagName].Value = val
-    setFlags(newFlags)
-  }
+    const newCurrFlag = { ...currFlag, Value: val }
 
+    setFlags(new Map(flags.set(flagName, newCurrFlag))) 
+  }
+  
   const reset = async () => {
     setOutput(undefined)
   }
 
   const run = async () => {
     setLoading({ loading: true, loadingText: `Minions in your computer are running the agents!` })
+
+    // sleep for 100ms for stability
+    await new Promise(r => setTimeout(r, 100));
+
     try {
       const res = await runGame()
       setOutput(res)
@@ -82,24 +100,33 @@ const NewRun = () => {
     setLoading(initialLoadingState)
   }
 
+  const getFlagForms = (fs: Map<string, Flag>): JSX.Element[] => {
+    let ret: JSX.Element[] = []
+    fs.forEach((f, n) => {
+      ret.push(
+        <FlagForm setFlag={(val) => setFlag(n, val)} flag={f} disabled={output !== undefined} />
+      )
+    })
+    return ret
+  }
+
   return <div className={styles.root}>
     <h1>{output ? `Run Output` : `New Run`}</h1>
 
     {
-      output ?
-        <div>
-          <Button variant="danger" size="lg" onClick={reset}>Reset</Button>
-        </div>
-        :
-        <div>
-          <Button variant="success" size="lg" onClick={run} disabled={flags === undefined}>Run</Button>
-        </div>
+      !output &&
+      <div>
+        <Button variant="success" size="lg" onClick={run} disabled={flags === undefined}>Run</Button>
+      </div>
     }
     {
       flags &&
-      <Row style={{ marginLeft: `5vw`, marginRight: `5vw`, marginTop: 48 }}>
-        {Object.entries(flags).map(([n, f]) => <FlagForm setFlag={(val) => setFlag(n, val)} flag={f} />)}
-      </Row>
+      <div>
+        <Button variant="danger" size="lg" onClick={loadFlags} disabled={output !== undefined} style={{ marginTop: 24 }}>Reset Flags</Button>
+        <Row style={{ marginLeft: `5vw`, marginRight: `5vw`, marginTop: 48, marginBottom: 48 }}>
+          {getFlagForms(flags)}
+        </Row>
+      </div>
     }
     {
       runError &&
@@ -110,14 +137,20 @@ const NewRun = () => {
     }
     {
       output &&
-      <div style={{ textAlign: `left`, padding: `0 3vw` }}>
-        <div style={{ marginBottom: 100 }}>
-          <h2><code>output.json</code></h2>
-          <CodeBlock text={JSON.stringify(output.output, null, "\t")} wrapLines showLineNumbers language="json" theme="dracula" />
+      <div style={{ marginTop: 48 }}>
+        <div>
+          <Button variant="danger" size="lg" onClick={reset}>Reset Run</Button>
         </div>
-        <div style={{ marginBottom: 100 }}>
-          <h2><code>log.txt</code></h2>
-          <CodeBlock text={output.logs} wrapLines showLineNumbers language="text" theme="dracula" />
+
+        <div style={{ textAlign: `left`, padding: `0 3vw` }}>
+          <div style={{ marginBottom: 100 }}>
+            <h2><code>output.json</code></h2>
+            <CodeBlock text={JSON.stringify(output.output, null, "\t")} wrapLines showLineNumbers language="json" theme="dracula" />
+          </div>
+          <div style={{ marginBottom: 100 }}>
+            <h2><code>log.txt</code></h2>
+            <CodeBlock text={output.logs} wrapLines showLineNumbers language="text" theme="dracula" />
+          </div>
         </div>
       </div>
     }
