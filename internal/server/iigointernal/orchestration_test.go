41578efed8735de641d2a013fea0d84cb7e7849c
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-12-30 00:44:51 +0100

    Added utilities tests

--- internal/server/iigointernal/orchestration_test.go
+++ internal/server/iigointernal/orchestration_test.go
@@ -35,13 +35,3 @@ func TestPutSalaryBack(t *testing.T) {
 		}
 	}
 }
-
-// we need more tests:
-// below are the test proposed:
-//1. test salary paid are moved into island private resources pools
-//2. test that the first round will works well
-//3. add experimentatl
-
-// Incure service charge in each file
-// DepositIntoClient private pool - Utilies
-// WithdrawFromCommonPool - Utilies?
