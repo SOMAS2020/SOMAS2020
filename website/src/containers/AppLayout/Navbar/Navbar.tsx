2ee07a52336576ebf5685b6812146c191a8720c4
Darrekt <dl5215@ic.ac.uk>
2020-12-29 17:31:09 +0000

    Add navbar links

--- website/src/containers/AppLayout/Navbar/Navbar.tsx
+++ website/src/containers/AppLayout/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo/logo192.png';
@@ -7,46 +7,53 @@ import outputJSONData from '../../../output/output.json'
 
 import styles from './Navbar.module.css'
 
-
 const AppNavbar = () => {
-    const [navExpanded, setNavExpanded] = useState(false)
+  const [navExpanded, setNavExpanded] = useState(false)
 
-    const closeNav = () => setNavExpanded(false)
+  const closeNav = () => setNavExpanded(false)
 
-    const getNavLink = (text: string, link: string) => 
-        <LinkContainer to={link} onClick={closeNav}>
-            <Nav.Link className="lightbluelink">{text}</Nav.Link>
-        </LinkContainer>
+  const getNavLink = (text: string, link: string) =>
+    <LinkContainer to={link} onClick={closeNav}>
+      <Nav.Link className="lightbluelink">{text}</Nav.Link>
+    </LinkContainer>
 
-    return <>
-        <Navbar fixed="top" bg="dark" variant="dark" expand="lg" 
-            onToggle={() => setNavExpanded(!navExpanded)} expanded={navExpanded}>
+  return <>
+    <Navbar fixed="top" bg="dark" variant="dark" expand="lg"
+      onToggle={() => setNavExpanded(!navExpanded)} expanded={navExpanded}>
 
-            <Link to="/" className={styles.enlargeOnHover}>
-                <Navbar.Brand>
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
+      <Link to="/" className={styles.enlargeOnHover}>
+        <Navbar.Brand>
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
                     SOMAS 2020
             </Navbar.Brand>
-            </Link>
+      </Link>
 
-            <a rel="noopener noreferrer" target="_blank" href={outputJSONData.GitInfo.GithubURL} className="lightbluelink">
-                {outputJSONData.GitInfo.Hash.substr(0, 7)}
-            </a>
+      <a rel="noopener noreferrer" target="_blank" href={outputJSONData.GitInfo.GithubURL} className="lightbluelink">
+        {outputJSONData.GitInfo.Hash.substr(0, 7)}
+      </a>
 
-
-            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={closeNav} />
-            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                <Nav className="mr-auto" />
-                {getNavLink("Raw Output", "/rawoutput")}
-            </Navbar.Collapse>
-        </Navbar>
-    </>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={closeNav} />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+        <Nav className="mr-auto" >
+          {getNavLink("Raw Output", "/rawoutput")}
+          <NavDropdown title="Visualisations" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/resources">Resources</NavDropdown.Item>
+            <NavDropdown.Item href="/roles">Roles by Turn</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">IIGO</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">IITO</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">IIFO</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </>
 }
 
 export default AppNavbar
