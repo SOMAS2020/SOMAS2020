10a0c2a6c50305bbfbe636ac05e89d33d2b4475a
lhl2617 <lhlee888@hotmail.com>
2020-12-29 17:17:26 +0000

    [VIS/WASM] (4) Add loading context and update README

---
+++ website/src/components/Loading/Loading.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+
+import loadingSVG from './Loading.svg'
+import { useLoadingState } from '../../contexts/loadingState'
+
+const Loading = () => {
+  const [loadingState] = useLoadingState()
+  const { loading, loadingText } = loadingState
+
+  return (
+    <Modal
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={loading}
+      onHide={() => {}}
+      style={{ textAlign: `center`, margin: `auto` }}
+    >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Loading...
+       </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* TODO:- Fix Freezing issue--might need web worker? */}
+        <img src={loadingSVG} alt="loading" className="loader" />
+        <p style={{ padding: 10, fontSize: '1.2rem' }}>{loadingText ? loadingText : `Tomato sauce was sold in the 1800's as medicine.`}</p>
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default Loading
