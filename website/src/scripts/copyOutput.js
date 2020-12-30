d0cab1be5f6dd44de7a1599e5a3f68c650703084
lhl2617 <lhlee888@hotmail.com>
2020-12-30 14:29:29 +0000

    [VIS] Overhaul copyOutput.js script

--- website/src/scripts/copyOutput.js
+++ website/src/scripts/copyOutput.js
@@ -1,13 +1,32 @@
+/**
+ * copyOutput.js
+ * 
+ * This script copies the output from the completed Go program simulation
+ * from the root of the repository.
+ * 
+ * You can run this script using `yarn copyoutput`.
+ * This script also runs during prebuild and prestart.
+ */
+
 const fse = require('fs-extra')
 const fs = require('fs')
+const path = require('path')
 
 try {
-    fse.copySync(`../output`, `src/output`)
+    const websiteRoot = path.dirname(path.dirname(__dirname))
+    const repoRoot = path.dirname(websiteRoot)
+
+    fse.copySync(
+        path.join(repoRoot, `output`), 
+        path.join(websiteRoot, `src`, `output`),
+    )
     
-    const logTxt = fs.readFileSync(`src/output/log.txt`, `utf8`)
+    const logTxt = fs.readFileSync(
+        path.join(websiteRoot, `src`, `output`, `log.txt`), 
+        `utf8`)
     const logLines = logTxt.split(`\n`)
     
-    fs.writeFileSync(`src/output/log.txt.json`, JSON.stringify(logLines))
+    fs.writeFileSync(path.join(websiteRoot, `src`, `output`, `log.txt.json`), JSON.stringify(logLines))
 }
 catch (err) {
     console.error(err)
