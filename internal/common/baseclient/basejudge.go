76952cfce36357fe41a5e89d159e07c9aeaf3fd1
Neelesh99 <Krishna1965!>
2020-12-30 11:23:46 +0000

    Changes for historical retribution tooling.

--- internal/common/baseclient/basejudge.go
+++ internal/common/baseclient/basejudge.go
@@ -68,6 +68,10 @@ func (j *BaseJudge) GetPardonedIslands(currentSanctions map[int][]roles.Sanction
 	return map[int][]bool{}
 }
 
+func (j *BaseJudge) HistoricalRetributionEnabled() bool {
+	return false
+}
+
 // PickUpRulesByVariable returns a list of rule_id's which are affected by certain variables.
 func PickUpRulesByVariable(variableName rules.VariableFieldName, ruleStore map[string]rules.RuleMatrix) ([]string, bool) {
 	var Rules []string
