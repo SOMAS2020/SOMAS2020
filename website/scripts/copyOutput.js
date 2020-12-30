b22a1068bbd0dedd899c1b67b00f38670880ad0e
lhl2617 <lhlee888@hotmail.com>
2020-12-30 15:04:23 +0000

    [VIS] Build output if not found in CopyOutput

--- website/scripts/copyOutput.js
+++ website/scripts/copyOutput.js
@@ -4,6 +4,8 @@
  * This script copies the output from the completed Go program simulation
  * from the root of the repository.
  * 
+ * If the output is not found, this scripts attempts to run the simulation.
+ * 
  * You can run this script using `yarn copyoutput`.
  * This script also runs during prebuild and prestart.
  */
@@ -11,10 +13,22 @@
 const fse = require('fs-extra')
 const fs = require('fs')
 const path = require('path')
+const cp = require('child_process')
 
 try {
     const websiteRoot = path.dirname(__dirname)
     const repoRoot = path.dirname(websiteRoot)
+
+    if (!fs.existsSync(path.join(repoRoot, `output`))) {
+        console.log("Did not find simulation output, running simulation...")
+        const stdout = cp.execSync(
+            `go run .`,
+            {
+                cwd: repoRoot,
+            }
+        )
+        console.log(stdout.toString())
+    }
 
     fse.copySync(
         path.join(repoRoot, `output`), 
