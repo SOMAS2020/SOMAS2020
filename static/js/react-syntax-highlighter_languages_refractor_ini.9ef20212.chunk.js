bf1408536d625b783261b73f8c1ed71e7e543121
yp717 <>
2020-12-31 15:13:40 +0000

    yp717 published a site update

--- static/js/react-syntax-highlighter_languages_refractor_ini.9ef20212.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[60],{422:function(t,s,e){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.9ef20212.chunk.js.map
