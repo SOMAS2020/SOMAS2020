821fe6db0b467bacc04f2830b1dc5149bd75a3f6
Darrekt <dl5215@ic.ac.uk>
2020-12-31 16:42:03 +0000

    Add barrel file for cleaner Content

--- website/src/containers/AppLayout/Content/Content.tsx
+++ website/src/containers/AppLayout/Content/Content.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Home from "../../../components/Home/Home"
 import { Route, Switch } from "react-router-dom"
 import {
   cioutput,
@@ -12,27 +11,22 @@ import {
   resourcesvisualisation
 } from "../../../consts/paths"
 
+import Home from "../../../components/Home/Home"
 import CIOutput from "../../../components/CIOutput/CIOutput"
 import NewRun from "../../../components/NewRun/NewRun"
-import GameVisualisation from "../../../components/Visualisations/Game/Game"
-import IIGOVisualisation from "../../../components/Visualisations/IIGO/IIGO"
-import IITOVisualisation from "../../../components/Visualisations/IITO/IITO"
-import IIFOVisualisation from "../../../components/Visualisations/IIFO/IIFO"
-import RolesVisualisation from "../../../components/Visualisations/Roles/Roles"
-import ResourcesVisualisation from "../../../components/Visualisations/Resources/Resources"
-
+import * as Visualisations from "../../../components/Visualisations/Visualisations"
 const Content = () => {
   return (
     <div>
       <Switch>
         <Route path={cioutput} exact component={CIOutput} />
         <Route path={newrun} exact component={NewRun} />
-        <Route path={gamevisualisation} exact component={GameVisualisation} />
-        <Route path={iigovisualisation} exact component={IIGOVisualisation} />
-        <Route path={iitovisualisation} exact component={IITOVisualisation} />
-        <Route path={iifovisualisation} exact component={IIFOVisualisation} />
-        <Route path={rolesvisualisation} exact component={RolesVisualisation} />
-        <Route path={resourcesvisualisation} exact component={ResourcesVisualisation} />
+        <Route path={gamevisualisation} exact component={Visualisations.Game} />
+        <Route path={iigovisualisation} exact component={Visualisations.IIGO} />
+        <Route path={iitovisualisation} exact component={Visualisations.IITO} />
+        <Route path={iifovisualisation} exact component={Visualisations.IIFO} />
+        <Route path={rolesvisualisation} exact component={Visualisations.Roles} />
+        <Route path={resourcesvisualisation} exact component={Visualisations.Resources} />
         <Route component={Home} />
       </Switch>
     </div>
