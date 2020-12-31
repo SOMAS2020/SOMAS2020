34f67e5a2e3dc6ee467c5609436a62889e0501db
JZh <39611884+Regnigh@users.noreply.github.com>
2020-12-31 08:37:03 +0000

    Providing a basic friendship system

--- internal/clients/team6/client.go
+++ internal/clients/team6/client.go
@@ -8,10 +8,44 @@ import (
 
 const id = shared.Team6
 
+const (
+	hostile Friendship = iota
+	neutral
+	friendly
+)
+
+type GiftReceivedHistory map[shared.ClientID]shared.GiftOffer
+
+type Friendship int
+
+type FriendshipWith map[shared.ClientID]Friendship
+
 func init() {
-	baseclient.RegisterClient(id, &client{BaseClient: baseclient.NewClient(id)})
+	baseclient.RegisterClient(
+		id,
+		&client{
+			BaseClient: baseclient.NewClient(id),
+			friendshipWith: FriendshipWith{
+				shared.Team1: neutral,
+				shared.Team2: neutral,
+				shared.Team3: neutral,
+				shared.Team4: neutral,
+				shared.Team5: neutral,
+			},
+		},
+	)
 }
 
 type client struct {
 	*baseclient.BaseClient
+
+	friendshipWith FriendshipWith
+}
+
+func (c *client) RaiseFriendshipLevel() {
+	// increase the friendship level with some other islands
+}
+
+func (c *client) LowerFriendshipLevel() {
+	// decrease the friendship level with some other islands
 }
