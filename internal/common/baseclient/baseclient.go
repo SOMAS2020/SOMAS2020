2302ed6a93dbf677d96f3990d121be6f438d1812
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2021-01-01 15:15:06 +0000

    Update baseclient.go

--- internal/common/baseclient/baseclient.go
+++ internal/common/baseclient/baseclient.go
@@ -8,7 +8,6 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/disasters"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/gamestate"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/roles"
-	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
