4f3bcb7c2c694d67f397aa3343751d650ca306b1
Augusta <61150248+ahm116@users.noreply.github.com>
2021-01-02 18:30:54 +0000

    Update website/src/components/visualisation/Resources/Resources.tsx
    Co-authored-by: lhl2617 <33488131+lhl2617@users.noreply.github.com>

--- website/src/components/visualisation/Resources/Resources.tsx
+++ website/src/components/visualisation/Resources/Resources.tsx
@@ -5,7 +5,7 @@ import LineRechartComponent from './LineGraph';
 const Resources = () => {
   return (
     <div className={styles.root}>
-      <h1>Resources over Time </h1>
+      <h1>Resources over Time</h1>
       <p>Select teams to show/hide by cicking the team at the top of the chart. 
          Use the slider at the bottom to change which turns are displayed. </p>
       <LineRechartComponent />
