1ba1a26f64ddab70e764317345b9d46e752f9512
ahm116 <ahm116@ic.ac.uk>
2021-01-02 20:18:28 +0000

    indents, spacing, module imports fixed. Object decomposition added

--- website/src/components/visualisation/Resources/LineGraph.tsx
+++ website/src/components/visualisation/Resources/LineGraph.tsx
@@ -1,71 +1,71 @@
 import React from "react";
-import { Brush, ResponsiveContainer, LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend, Surface, Symbols, ReferenceLine } from '../../../../node_modules/recharts';
+import { Brush, ResponsiveContainer, LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend, Surface, Symbols, ReferenceLine } from 'recharts';
 import outputJSON from '../../../output/output.json';
-import _ from "../../../../node_modules/lodash";
+import _ from "lodash";
 
 
 class LineRechartComponent extends React.Component<{}, any> {
 
-    //data = outputJSON;
-
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             disabled: [],
             lineColours: {
-                team1: "#0095FF", 
-                team2: "#FF0000", 
-                team3: "#802FF0", 
-                team4: "#00C49F", 
-                team5: "#FFBB28", 
-                team6: "#FF8042", 
-            }, 
+                team1: "#0095FF",
+                team2: "#FF0000",
+                team3: "#802FF0",
+                team4: "#00C49F",
+                team5: "#FFBB28",
+                team6: "#FF8042",
+            },
             datapaths: {
-                team1: "ClientInfos.Team1.Resources", 
-                team2: "ClientInfos.Team2.Resources", 
-                team3: "ClientInfos.Team3.Resources", 
-                team4: "ClientInfos.Team4.Resources", 
-                team5: "ClientInfos.Team5.Resources", 
-                team6: "ClientInfos.Team6.Resources", 
+                team1: "ClientInfos.Team1.Resources",
+                team2: "ClientInfos.Team2.Resources",
+                team3: "ClientInfos.Team3.Resources",
+                team4: "ClientInfos.Team4.Resources",
+                team5: "ClientInfos.Team5.Resources",
+                team6: "ClientInfos.Team6.Resources",
             },
             chartData: outputJSON
         };
     }
 
     handleClick = dataKey => {
-        if(_.includes(this.state.disabled, dataKey)) {
+        const { disabled } = this.state;
+        if (disabled.includes(dataKey)) {
             this.setState({
                 disabled: this.state.disabled.filter(obj => obj !== dataKey)
             });
         }
         else {
             this.setState({
-                disabled:this.state.disabled.concat(dataKey)
+                disabled: this.state.disabled.concat(dataKey)
             });
         }
     };
 
     renderCustomizedLegend = ({ payload }) => {
-        return(
+        return (
             <div className="customized-legend">
                 {payload.map(entry => {
                     const { dataKey, colour } = entry;
-                    const active = _.includes(this.state.disabled, dataKey);
+                    const { disabled } = this.state;
+                    const active = disabled.includes(dataKey);
                     const style = {
-                        marginRight: 10, 
+                        marginRight: 10,
                         colour: active ? "#AAA" : "#000"
                     };
 
-                    return(
+                    return (
                         <span
-                            className="legend-item" 
+                            className="legend-item"
                             onClick={() => this.handleClick(dataKey)}
                             style={style}
                         >
                             <Surface width={10} height={10} viewBox="0 0 10 10" >
                                 <Symbols cx={5} cy={5} type="circle" size={50} fill={colour} />
                                 {active && (
-                                    <Symbols 
+                                    <Symbols
                                         cx={5}
                                         cy={5}
                                         type="circle"
@@ -82,12 +82,12 @@ class LineRechartComponent extends React.Component<{}, any> {
         );
     };
 
-    getSeasonEnds () {
+    getSeasonEnds() {
         var seasonEnds: any[] = [];
         let i;
-        for(i in this.state.chartData.GameStates) {
-            if(this.state.chartData.GameStates[i].Environment.LastDisasterReport.Magnitude !== 0){
-                seasonEnds.push(this.state.chartData.GameStates[i].Turn -1)
+        for (i in this.state.chartData.GameStates) {
+            if (this.state.chartData.GameStates[i].Environment.LastDisasterReport.Magnitude !== 0) {
+                seasonEnds.push(this.state.chartData.GameStates[i].Turn - 1)
             }
         }
         return seasonEnds;
@@ -95,25 +95,25 @@ class LineRechartComponent extends React.Component<{}, any> {
 
     render() {
         return (
-            <ResponsiveContainer height={330} width = "95%">
+            <ResponsiveContainer height={330} width="95%">
                 <LineChart data={this.state.chartData.GameStates}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     {_.toPairs(this.state.lineColours)
-                    .filter(pair => !_.includes(this.state.disabled, pair[0]))
-                    .map(pair => (
-                    <Line
-                    name={pair[0]}
-                    type="monotone"
-                    dataKey={this.state.datapaths[pair[0]]}
-                    stroke={pair[1]}
-                    />
-                    ))}
+                        .filter(pair => !_.includes(this.state.disabled, pair[0]))
+                        .map(pair => (
+                            <Line
+                                name={pair[0]}
+                                type="monotone"
+                                dataKey={this.state.datapaths[pair[0]]}
+                                stroke={pair[1]}
+                            />
+                        ))}
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="Turn" height={50} label={{ value: 'Turn', angle: 0, position: 'insideBottom', dy: -8}} />
-                    <YAxis label={{ value: 'Resources', angle: -90, position: 'insideLeft' }}/>
+                    <XAxis dataKey="Turn" height={50} label={{ value: 'Turn', angle: 0, position: 'insideBottom', dy: -8 }} />
+                    <YAxis label={{ value: 'Resources', angle: -90, position: 'insideLeft' }} />
                     <Tooltip />
-                    {this.getSeasonEnds().map((seasonEnd) => 
-                        (
+                    {this.getSeasonEnds().map((seasonEnd) =>
+                    (
                         <ReferenceLine
                             x={seasonEnd}
                             label="Season End"
@@ -121,27 +121,23 @@ class LineRechartComponent extends React.Component<{}, any> {
                             strokeDasharray="3 3"
                         />
                     ))}
-                    <Legend 
-                        verticalAlign="top" 
+                    <Legend
+                        verticalAlign="top"
                         align="center"
-                        height={20} 
-                        wrapperStyle={{top: 0, left: 25, right: 0, width: 'auto'}}
+                        height={20}
+                        wrapperStyle={{ top: 0, left: 25, right: 0, width: 'auto' }}
                         payload={_.toPairs(this.state.lineColours).map(pair => ({
-                            dataKey: pair[0], 
+                            dataKey: pair[0],
                             colour: pair[1],
                         }))}
-                        
+
                         content={this.renderCustomizedLegend}
                     />
-                    <Brush dataKey='Turn' height={28} stroke="#2fa1c6"/>
+                    <Brush dataKey='Turn' height={25} stroke="#2fa1c6" />
                 </LineChart>
             </ResponsiveContainer >
         )
     };
 }
 
-
 export default LineRechartComponent;
-
-
-
--- website/src/components/visualisation/Resources/Resources.tsx
+++ website/src/components/visualisation/Resources/Resources.tsx
@@ -5,9 +5,9 @@ import LineRechartComponent from './LineGraph';
 const Resources = () => {
   return (
     <div className={styles.root}>
-      <h1>Resources over Time </h1>
-      <p>Select teams to show/hide by cicking the team at the top of the chart. 
-         Use the slider at the bottom to change which turns are displayed. </p>
+      <h1>Resources over Time</h1>
+      <p>Select teams to show/hide by cicking the team at the top of the chart.
+         Use the slider at the bottom to change which turns are displayed.</p>
       <LineRechartComponent />
     </div>
   );
