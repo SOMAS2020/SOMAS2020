248c82b35d18517607675b5ef6aeed9b3d973827
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2021-01-02 16:02:20 +0000

    Update election_test.go

--- internal/common/voting/election_test.go
+++ internal/common/voting/election_test.go
@@ -20,10 +20,10 @@ func TestElection(t *testing.T) {
 		{shared.Team2, shared.Team5, shared.Team6, shared.Team4, shared.Team3, shared.Team1},
 		{shared.Team6, shared.Team4, shared.Team1, shared.Team5, shared.Team2, shared.Team3},
 		{shared.Team5, shared.Team2, shared.Team3, shared.Team6, shared.Team1, shared.Team4}}
-	//fmt.Println("Initializing struct Election done. ")
+	fmt.Println("Initializing struct Election done. ")
 	//clientMap := make(map[shared.ClientID]baseclient.Client)
 
-	//ele.bordaCountResult()
+	ele.bordaCountResult()
 	//ele.runOffResult(clientMap)
 	//ele.instantRunoffResult(clientMap)
 	//ele.approvalResult()
