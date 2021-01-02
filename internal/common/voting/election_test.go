94a345e0c5357edf98f95ac8e09a3991e7d5611a
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2021-01-02 16:34:28 +0000

    Update election_test.go

--- internal/common/voting/election_test.go
+++ internal/common/voting/election_test.go
@@ -1,7 +1,6 @@
 package voting
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
@@ -20,7 +19,6 @@ func TestElection(t *testing.T) {
 		{shared.Team2, shared.Team5, shared.Team6, shared.Team4, shared.Team3, shared.Team1},
 		{shared.Team6, shared.Team4, shared.Team1, shared.Team5, shared.Team2, shared.Team3},
 		{shared.Team5, shared.Team2, shared.Team3, shared.Team6, shared.Team1, shared.Team4}}
-	fmt.Println("Initializing struct Election done. ")
 	clientMap := make(map[shared.ClientID]baseclient.Client)
 
 	var c baseclient.BaseClient
@@ -28,17 +26,10 @@ func TestElection(t *testing.T) {
 	for i := 0; i < len(ele.voterList); i++ {
 		clientMap[ele.voterList[i]] = &c
 	}
-	fmt.Println(clientMap)
-	list := clientMap[ele.voterList[1]].VoteForElection(ele.roleToElect, ele.candidateList)
-	fmt.Println(list)
 
 	winner := ele.bordaCountResult()
-	fmt.Println(winner)
 	winner = ele.runOffResult(clientMap)
-	fmt.Println(winner)
 	winner = ele.instantRunoffResult(clientMap)
-	fmt.Println(winner)
 	winner = ele.approvalResult()
-	fmt.Println(winner)
 
 }
