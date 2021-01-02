5a9b9985b19033a7f59067ba68f37999a9d07302
Olly Larkin <ollylemonlarkin@gmail.com>
2021-01-02 21:17:04 +0000

    [VIS] updated data processing to fetch team names from AuxInfo

--- website/src/components/Visualisations/Roles/Util/ProcessedRoleData.ts
+++ website/src/components/Visualisations/Roles/Util/ProcessedRoleData.ts
@@ -1,4 +1,4 @@
-import outputJSON from "../../../../output/output.json";
+import { OutputJSONType } from "../../../../consts/types";
 import {
     ProcessedRoleData,
     ProcessedRoleElement,
@@ -20,13 +20,14 @@ const standardise = (allRoles: ProcessedRoleData): ProcessedRoleData => {
     });
 };
 
-export const processRoleData = (data: typeof outputJSON) => {
+export const processRoleData = (data: OutputJSONType) => {
     if (data.GameStates.length === 0) return [];
 
     let allRoles: ProcessedRoleData = [];
 
-    for (var id in data.GameStates[0].ClientInfos)
-        allRoles.push(new ProcessedRoleElement(id));
+    data.AuxInfo.TeamIDs.map((id) =>
+        allRoles.push(new ProcessedRoleElement(id))
+    );
 
     if (allRoles.length === 0) return [];
 
