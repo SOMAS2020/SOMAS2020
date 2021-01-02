5ee66f9de355bf2a4d94eed6f59724afd81b2e39
zx516 <zx516@imperial.ac.uk>
2021-01-02 19:40:25 +0000

    custom-client fresh start

--- internal/clients/team4/client.go
+++ internal/clients/team4/client.go
@@ -2,8 +2,12 @@
 package team4
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
+	"gonum.org/v1/gonum/mat"
 )
 
 const id = shared.Team4
@@ -13,5 +17,43 @@ func init() {
 }
 
 type client struct {
-	*baseclient.BaseClient
+	*baseclient.BaseClient //client struct has access to methods and fields of the BaseClient struct which implements implicitly the Client interface.
+
+	//custom fields
+	yes         string                  //this field is just for testing
+	obs         *observation            //observation is the raw input into our client
+	internalRep *internalRepresentation //internal state is condensed from observation
+}
+
+type observation struct {
+}
+
+type internalRepresentation struct {
+	trustMatrix *mat.Dense
+}
+
+//Overriding the Initialise method of the BaseClient to initilise the trust matrix too
+func (c *client) Initialise(serverReadHandle baseclient.ServerReadHandle) {
+	c.ServerReadHandle = serverReadHandle
+
+	//custom things below, trust matrix initilised to values of 1
+	numClient := len(shared.TeamIDs)
+	v := make([]float64, numClient*numClient)
+	for i := range v {
+		v[i] = 1
+	}
+	c.internalRep = &internalRepresentation{
+		trustMatrix: mat.NewDense(numClient, numClient, v),
+	}
+}
+
+//Overriding the StartOfTurn method of the BaseClient
+func (c *client) StartOfTurn() {
+	c.yes = "yes"
+	c.Logf(`what are you doing?
+	=========================================
+	==============================================
+	================================================`)
+	c.Logf("this is a %v for you ", c.yes)
+	fmt.Println(reflect.TypeOf(c))
 }
