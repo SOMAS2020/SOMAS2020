abf562e89a1c0000ab40a4aecaa28d55232e818e
Neelesh99 <Krishna1965!>
2021-01-03 12:58:21 +0000

    Added linked rule evaluation test

--- internal/common/rules/basicruleevaluator_test.go
+++ internal/common/rules/basicruleevaluator_test.go
@@ -31,9 +31,44 @@ func TestBasicRealValuedRuleEvaluator(t *testing.T) {
 	}
 }
 
+func TestBasicLinkedRuleEvaluator(t *testing.T) {
+	registerNewLinkedRule(t)
+	result, err := BasicLinkedRuleEvaluator("Linked test rule")
+	if err != nil {
+		t.Errorf("Linked rule evaluation error: %v", err)
+	}
+	if !result {
+		t.Errorf("Linked rule evaluated to %v expected %v", result, true)
+	}
+}
+
 func registerNewRealValuedRule(t *testing.T) {
 	//A very contrived rule//
 	name := "Real Test rule"
+	reqVar := []VariableFieldName{
+		NumberOfIslandsContributingToCommonPool,
+		NumberOfFailedForages,
+		NumberOfBrokenAgreements,
+		MaxSeverityOfSanctions,
+	}
+
+	v := []float64{1, 0, 0, 0, -4, 0, -1, -1, 0, 2, 0, 0, 0, 0, 2, 0, 0, 1, 0, -1}
+	CoreMatrix := mat.NewDense(4, 5, v)
+	aux := []float64{1, 1, 4, 0}
+	AuxiliaryVector := mat.NewVecDense(4, aux)
+
+	_, ruleError := RegisterNewRule(name, reqVar, *CoreMatrix, *AuxiliaryVector, false, RuleLink{
+		Linked: false,
+	})
+	if ruleError != nil {
+		t.Errorf("Problem with registering new real valued rule in test, error message : '%v'", ruleError.Error())
+	}
+	// Check internal/clients/team3/client.go for an implementation of a basic evaluator for this rule
+}
+
+func registerNewLinkedRule(t *testing.T) {
+	registerTestRule(AvailableRules)
+	name := "Linked test rule"
 	reqVar := []VariableFieldName{
 		NumberOfIslandsContributingToCommonPool,
 		NumberOfFailedForages,
@@ -47,10 +82,11 @@ func registerNewRealValuedRule(t *testing.T) {
 	AuxiliaryVector := mat.NewVecDense(4, aux)
 
 	_, ruleError := RegisterNewRule(name, reqVar, *CoreMatrix, *AuxiliaryVector, false, RuleLink{
-		Linked: false,
+		Linked:     true,
+		LinkType:   ParentFailAutoRulePass,
+		LinkedRule: "Kinda Complicated Rule",
 	})
 	if ruleError != nil {
 		t.Errorf("Problem with registering new real valued rule in test, error message : '%v'", ruleError.Error())
 	}
-	// Check internal/clients/team3/client.go for an implementation of a basic evaluator for this rule
 }
