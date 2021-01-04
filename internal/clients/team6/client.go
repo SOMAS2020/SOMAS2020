955b8b6c5dd920472f546b20676e10b85f96704e
JZh <39611884+Regnigh@users.noreply.github.com>
2021-01-04 09:14:12 +0000

    implemented iito funcs

--- internal/clients/team6/client.go
+++ internal/clients/team6/client.go
@@ -11,27 +11,17 @@ import (
 const (
 	id = shared.Team6
 	// MinFriendship is the minimum friendship level
-	MinFriendship = 0
+	MinFriendship = 0.0
 	// MaxFriendship is the maximum friendship level
-	MaxFriendship = 100
+	MaxFriendship = 100.0
 )
-
-// GiftsReceivedHistory is what gifts we have received from other islands
-type GiftsReceivedHistory map[shared.ClientID]shared.Resources
-
-// GiftsOfferedHistory is what gifts we have offered to other islands
-type GiftsOfferedHistory map[shared.ClientID]shared.Resources
-
-// Friendship indicates friendship level with other islands
-type Friendship map[shared.ClientID]uint
-
-// Config configures our island's initial state
-type Config struct {
-	friendship Friendship
-}
 
 type client struct {
 	*baseclient.BaseClient
+
+	friendship           map[shared.ClientID]float64
+	giftsOfferedHistory  map[shared.ClientID]shared.Resources
+	giftsReceivedHistory map[shared.ClientID]shared.Resources
 
 	config Config
 }
@@ -40,8 +30,11 @@ func init() {
 	baseclient.RegisterClient(
 		id,
 		&client{
-			BaseClient: baseclient.NewClient(id),
-			config:     configInfo,
+			BaseClient:           baseclient.NewClient(id),
+			friendship:           friendship,
+			giftsOfferedHistory:  giftsOfferedHistory,
+			giftsReceivedHistory: giftsReceivedHistory,
+			config:               config,
 		},
 	)
 }
@@ -63,13 +56,59 @@ func (c *client) ForageUpdate(forageDecision shared.ForageDecision, resources sh
 // ######    IITO    ######
 // ########################
 
+func (c *client) GetGiftOffers(receivedRequests shared.GiftRequestDict) shared.GiftOfferDict {
+	offers := shared.GiftOfferDict{}
+	ourResources := c.ServerReadHandle.GetGameState().ClientInfo.Resources
+	ourStatus := c.ServerReadHandle.GetGameState().ClientInfo.LifeStatus
+	ourPersonality := c.getPersonality()
+	friendshipCoffesOnOffer := make(map[shared.ClientID]shared.GiftOffer)
+
+	for team, fsc := range c.getFriendshipCoeffs() {
+		friendshipCoffesOnOffer[team] = shared.GiftOffer(fsc)
+	}
+
+	if ourStatus == shared.Critical {
+		// rejects all offers if we are in critical status
+		return offers
+	}
+
+	for team, status := range c.ServerReadHandle.GetGameState().ClientLifeStatuses {
+		amountOffer := shared.GiftOffer(0.0)
+
+		if status == shared.Critical {
+			// offers a minimum resources to all islands which are in critical status
+			// TODO: what is the best amount to offer???
+			offers[team] = shared.GiftOffer(10.0)
+		} else if amountRequested, found := receivedRequests[team]; found {
+			// otherwise gifts will be offered based on our friendship and the amount
+			if shared.Resources(amountRequested) > ourResources/6.0 {
+				amountOffer = shared.GiftOffer(ourResources / 6.0)
+			} else {
+				amountOffer = shared.GiftOffer(amountRequested)
+			}
+
+			if ourPersonality == Selfish {
+				// introduces high penalty on the gift offers if we are selfish
+				offers[team] = shared.GiftOffer(friendshipCoffesOnOffer[team] * friendshipCoffesOnOffer[team] * amountOffer)
+			} else if ourPersonality == Normal {
+				// introduces normal penalty
+				offers[team] = shared.GiftOffer(friendshipCoffesOnOffer[team] * amountOffer)
+			} else {
+				// intorduces no penalty - we are rich!
+				offers[team] = shared.GiftOffer(amountOffer)
+			}
+
+		}
+	}
+
+	// TODO: implement different wealth state for our island so we can decided --finished
+	// whether to be generous or not
+	return offers
+}
+
 // ------ TODO: COMPULSORY ------
 func (c *client) GetGiftRequests() shared.GiftRequestDict {
 	return c.BaseClient.GetGiftRequests()
-}
-
-func (c *client) GetGiftOffers(receivedRequests shared.GiftRequestDict) shared.GiftOfferDict {
-	return c.BaseClient.GetGiftOffers(receivedRequests)
 }
 
 func (c *client) GetGiftResponses(receivedOffers shared.GiftOfferDict) shared.GiftResponseDict {
@@ -86,6 +125,10 @@ func (c *client) SentGift(sent shared.Resources, to shared.ClientID) {
 
 func (c *client) ReceivedGift(received shared.Resources, from shared.ClientID) {
 
+}
+
+func (c *client) DecideGiftAmount(toTeam shared.ClientID, giftOffer shared.Resources) shared.Resources {
+	return c.BaseClient.DecideGiftAmount(toTeam, giftOffer)
 }
 
 // ########################
@@ -150,22 +193,45 @@ func (c *client) DisasterNotification(dR disasters.DisasterReport, effects disas
 
 // increases the friendship level with some other islands
 func (c *client) raiseFriendshipLevel(clientID shared.ClientID) {
-	currFriendship := c.config.friendship[clientID]
+	currFriendship := c.friendship[clientID]
 
 	if currFriendship == MaxFriendship {
 		return
 	}
 
-	c.config.friendship[clientID]++
+	c.friendship[clientID]++
 }
 
 // decreases the friendship level with some other islands
 func (c *client) lowerFriendshipLevel(clientID shared.ClientID) {
-	currFriendship := c.config.friendship[clientID]
+	currFriendship := c.friendship[clientID]
 
 	if currFriendship == MinFriendship {
 		return
 	}
 
-	c.config.friendship[clientID]--
+	c.friendship[clientID]--
+}
+
+// gets friendship cofficients
+func (c client) getFriendshipCoeffs() map[shared.ClientID]float64 {
+	friendshipCoeffs := make(map[shared.ClientID]float64)
+
+	for team, fs := range c.friendship {
+		friendshipCoeffs[team] = fs / MaxFriendship
+	}
+
+	return friendshipCoeffs
+}
+
+func (c client) getPersonality() Personality {
+	ourResources := c.ServerReadHandle.GetGameState().ClientInfo.Resources
+
+	if ourResources <= c.config.selfishThreshold {
+		return Selfish
+	} else if ourResources <= c.config.normalThreshold {
+		return Normal
+	}
+
+	return Generous
 }
