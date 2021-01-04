aec2154fc09ac8fc8e0653886db178a0b06037d4
Agrim Manchanda <agrim.manchanda@hotmail.co.uk>
2021-01-04 01:13:13 +0000

    GetGiftResponses implementatio

--- internal/clients/team3/clientiitoiifo.go
+++ internal/clients/team3/clientiitoiifo.go
@@ -1,11 +1,6 @@
 package team3
 
-// import (
-// 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
-// 	"github.com/SOMAS2020/SOMAS2020/internal/common/roles"
-// 	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
-// 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
-// )
+import "github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 
 /*
 	//IIFO: OPTIONAL
@@ -17,12 +12,32 @@ package team3
 	//IITO: COMPULSORY
 	GetGiftRequests() shared.GiftRequestDict
 	GetGiftOffers(receivedRequests shared.GiftRequestDict) shared.GiftOfferDict
-	GetGiftResponses(receivedOffers shared.GiftOfferDict) shared.GiftResponseDict
 	UpdateGiftInfo(receivedResponses shared.GiftResponseDict)
+	DecideGiftAmount(toTeam shared.ClientID, giftOffer shared.Resources) shared.Resources
 
-	//TODO: THESE ARE NOT DONE yet, how do people think we should implement the actual transfer?
+	//TODO: IITO NON COMPULSORY
 	// The server should handle the below functions maybe?
-	SentGift(sent shared.Resources, to shared.ClientID)
-	ReceivedGift(received shared.Resources, from shared.ClientID)
+	func (c *BaseClient) SentGift(sent shared.Resources, to shared.ClientID)
+	func (c *BaseClient) ReceivedGift(received shared.Resources, from shared.ClientID)
 
 */
+
+// GetGiftResponses returns the result of our island accepting/rejecting offered amounts.
+// Strategy: we accept all amounts except if the offering island(s) is/are critical, then we
+// do not take any of their offered amount(s).
+func (c *client) GetGiftResponses(receivedOffers shared.GiftOfferDict) shared.GiftResponseDict {
+	responses := shared.GiftResponseDict{}
+	for client, offer := range receivedOffers {
+		responses[client] = shared.GiftResponse{
+			AcceptedAmount: shared.Resources(offer),
+			Reason:         shared.Accept,
+		}
+		if c.isClientStatusCritical(client) {
+			responses[client] = shared.GiftResponse{
+				AcceptedAmount: shared.Resources(0),
+				Reason:         shared.DeclineDontNeed,
+			}
+		}
+	}
+	return responses
+}
