b6598e121130fa6371a7c286de87fcec87b6dcf0
victorbadoual <vb3917@ic.ac.uk>
2021-01-04 05:32:04 +0100

    Add first implemntation of DecideVote(0 in Speaker object

--- internal/clients/team3/speaker.go
+++ internal/clients/team3/speaker.go
@@ -2,6 +2,7 @@ package team3
 
 import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/roles"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
@@ -17,4 +18,14 @@ type speaker struct {
 func (s *speaker) PayJudge(salary shared.Resources) (shared.Resources, bool) {
 	// Use the base implementation
 	return s.BaseSpeaker.PayJudge(salary)
+}
+
+func (s *speaker) DecideVote(ruleID string, aliveClients []shared.ClientID) (string, []shared.ClientID, bool) {
+	var chosenClients []shared.ClientID
+	for _, islandID := range aliveClients {
+		if s.c.iigoInfo.sanctions.islandSanctions[islandID] != roles.NoSanction {
+			chosenClients = append(chosenClients, islandID)
+		}
+	}
+	return ruleID, chosenClients, true
 }
