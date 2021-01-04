e54fbf9fee7b76dbd7268a9257108c602a848931
victorbadoual <vb3917@ic.ac.uk>
2021-01-04 05:15:35 +0100

    Add first implementation of DecideForage()

--- internal/clients/team3/clientforaging.go
+++ internal/clients/team3/clientforaging.go
@@ -1,15 +1,33 @@
 package team3
 
-// import (
-// "github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
-// "github.com/SOMAS2020/SOMAS2020/internal/common/roles"
-// "github.com/SOMAS2020/SOMAS2020/internal/common/rules"
-// "github.com/SOMAS2020/SOMAS2020/internal/common/shared"
-// )
+import (
+	"math"
+	// "github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
+	// "github.com/SOMAS2020/SOMAS2020/internal/common/roles"
+	// "github.com/SOMAS2020/SOMAS2020/internal/common/rules"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
+)
 
 /*
 	DecideForage() (shared.ForageDecision, error)
 	ForageUpdate(shared.ForageDecision, shared.Resources)
 */
 
+func (c *client) DecideForage() (shared.ForageDecision, error) {
 
+	safetyFactor := 1.0 + (0.5/100)*c.params.riskFactor
+
+	//we want to have more than the critical threshold leftover after foraging
+	var minimumLeftoverResources = float64(c.criticalStatePrediction.upperBound) * safetyFactor
+
+	var foragingInvestment = 0.0
+	//for now we invest everything we can, because foraging is iffy.
+	if c.ServerReadHandle.GetGameState().ClientInfo.LifeStatus == shared.Alive {
+		foragingInvestment = math.Max(float64(c.ServerReadHandle.GetGameState().ClientInfo.Resources)-minimumLeftoverResources, 0)
+	}
+
+	return shared.ForageDecision{
+		Type:         shared.DeerForageType,
+		Contribution: shared.Resources(foragingInvestment),
+	}, nil
+}
