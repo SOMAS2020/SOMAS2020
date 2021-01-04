00929bc9c3d0ff1e3c8d79260ebbf01ade2730c8
Yannis Panagis <yannis.panagis1998@gmail.com>
2021-01-04 11:48:54 +0000

    progress update

--- internal/clients/team2/forage.go
+++ internal/clients/team2/forage.go
@@ -65,8 +65,7 @@ func (c *client) ReceiveForageInfo(neighbourForaging []shared.ForageShareInfo) {
 func decideThreshold(c *client) float64 { //will move the threshold, higher value means more likely to hunt
 	if Otheragentinfo(c)==1{    //in the case when one other person only is hunting  
 		return 0.95 
-	}
-	else if Otheragentinfo(c)>1{   //if no one is likely to hunt then we do default probability
+	}else if Otheragentinfo(c)>1{   //if no one is likely to hunt then we do default probability
 		return 0.1 + (Otheragentinfo(c) * 0.15) //default hunt probability is 10%, the less people hunting the more likely we do it
 	}else{
 		return 0.1  //when no one is likely to hunt we have a default 10% chance of hunting just in the off chance another person hunts
@@ -81,6 +80,7 @@ func Otheragentinfo(c *client) float64 { //will return a value of how many agent
 		var agentHistory [ClientId]float64   //this map will store the sum of the decisions made by this agent e.g. 3 turns and a sum of 3 means they hunted every round
 	}
 	for i:=0; i< ; i++ {	//update agentHistory by looping through all the agents and adding their most recent decisions and then find their average hunt/fish
+		//c.foragingReturnHist to call the foraging 
 		[ClientId]agentHistory + //whatever the agent declares 0,1
 		if [ClientId]agentHistory/c.gameState().Turn>0.5{  //if an agent on average picks hunt more than 50% of the time we assume they will pick hunt
 			Huntnum++
@@ -89,4 +89,25 @@ func Otheragentinfo(c *client) float64 { //will return a value of how many agent
 	return HuntNum
 
 	return 0
+}
+
+func (c *client) ReceiveForageInfo(neighbourForaging []shared.ForageShareInfo) {
+	// Return on Investment
+	roi := map[shared.ClientID]shared.Resources{}
+	for _, val := range neighbourForaging {
+		c.foragingReturnsHist[val.SharedFrom].append(shared.ForageInfo{	DecisionMade : val.DecisionMade,                 //this needs to change
+																		ResourcesObtained : val.ResourcesObtained})
+
+		if val.DecisionMade.Type == shared.DeerForageType {
+			roi[val.SharedFrom] = val.ResourceObtained / shared.Resources(val.DecisionMade.Contribution) * 100
+		}
+	}
+}
+
+// MakeForageInfo allows clients to share their most recent foraging DecisionMade, ResourceObtained from it to
+// other clients.
+// OPTIONAL. If this is not implemented then all values are nil.
+func (c *BaseClient) MakeForageInfo() shared.ForageShareInfo {
+	contribution := shared.ForageDecision{Type: shared.DeerForageType, Contribution: 0}
+	return shared.ForageShareInfo{DecisionMade: contribution, ResourceObtained: 0, ShareTo: []shared.ClientID{}}
 }
