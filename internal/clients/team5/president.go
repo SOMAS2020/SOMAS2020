ae986ba941127e35d9a4483ea976d1164aa12269
P-A-Ha <philipposhadjimarkou@gmail.com>
2021-01-05 14:36:55 +0000

    updated president to work with new iigo

--- internal/clients/team5/president.go
+++ internal/clients/team5/president.go
@@ -15,17 +15,13 @@ type president struct {
 	c *client
 }
 
-//This I'm not 100% certain
-
 func (c *client) GetClientPresidentPointer() roles.President {
 	c.Logf("Team 5 is now the President, Shalom to all")
 
 	return &c.team5president
 }
 
-// EvaluateAllocationRequests sets allowed resource allocation based on each islands requests
-// COMPULSORY: decide how much each island can take from the common pool.
-func (pres *president) EvaluateAllocationRequests(resourceRequest map[shared.ClientID]shared.Resources, availCommonPool shared.Resources) (map[shared.ClientID]shared.Resources, bool) {
+func (pres *president) EvaluateAllocationRequests(resourceRequest map[shared.ClientID]shared.Resources, availCommonPool shared.Resources) shared.PresidentReturnContent {
 	var requestSum shared.Resources
 	resourceAllocation := make(map[shared.ClientID]shared.Resources)
 
@@ -48,19 +44,32 @@ func (pres *president) EvaluateAllocationRequests(resourceRequest map[shared.Cli
 			resourceAllocation[id] = 0
 		}
 	}
-	return resourceAllocation, true
+
+	return shared.PresidentReturnContent{
+		ContentType: shared.PresidentAllocation,
+		ResourceMap: resourceAllocation,
+		ActionTaken: true,
+	}
 }
 
 //TODO: NEED TO LOOK AT THE RULES
 // PickRuleToVote chooses a rule proposal from all the proposals
-// COMPULSORY: decide a method for picking a rule to vote on
-//NOT SURE ABOUT THISSSSSSS
-func (pres *president) PickRuleToVote(rulesProposals []string) (string, bool) {
-	if len(rulesProposals) == 0 {
-		// No rules were proposed by the islands
-		return "", false
+func (pres *president) PickRuleToVote(rulesProposals []string) shared.PresidentReturnContent {
+	// DefaulContentType: No rules were proposed by the islands
+	proposedRule := ""
+	actionTaken := false
+
+	// if some rules were proposed
+	if len(rulesProposals) != 0 {
+		proposedRule = rulesProposals[rand.Intn(len(rulesProposals))]
+		actionTaken = true
 	}
-	return rulesProposals[rand.Intn(len(rulesProposals))], true
+
+	return shared.PresidentReturnContent{
+		ContentType:  shared.PresidentRuleProposal,
+		ProposedRule: proposedRule,
+		ActionTaken:  actionTaken,
+	}
 }
 
 // SetTaxationAmount sets taxation amount for all of the living islands
@@ -68,35 +77,49 @@ func (pres *president) PickRuleToVote(rulesProposals []string) (string, bool) {
 // COMPULSORY: decide how much to tax islands, using rules as a guide if you wish.
 
 //Essentially the more the resources the greater the applied tax, as the point of the game is to survive
-func (pres *president) SetTaxationAmount(islandsResources map[shared.ClientID]shared.Resources) (map[shared.ClientID]shared.Resources, bool) {
+func (pres *president) SetTaxationAmount(islandsResources map[shared.ClientID]shared.ResourcesReport) shared.PresidentReturnContent {
 	var totalrecleft shared.Resources
 	taxAmountMap := make(map[shared.ClientID]shared.Resources)
 
 	for _, t := range islandsResources { //sum of all available resources
-		totalrecleft += t
+		totalrecleft += t.ReportedAmount
 	}
 
 	for id, resourceLeft := range islandsResources {
-		taxAmountMap[id] = (resourceLeft * resourceLeft / totalrecleft) //instead of applying random taxation, the taxation is now directly proportional to each islands remaining resources/total remaining resources
-
+		if resourceLeft.Reported {
+			taxAmountMap[id] = (resourceLeft.ReportedAmount * resourceLeft.ReportedAmount / totalrecleft) //instead of applying random taxation, the taxation is now directly proportional to each islands remaining resources/total remaining resources
+		} else {
+			taxAmountMap[id] = 20 // Flat tax rate which high enough, in order to encourage islands to share their resources
+		}
 	}
-	return taxAmountMap, true
+	return shared.PresidentReturnContent{
+		ContentType: shared.PresidentTaxation,
+		ResourceMap: taxAmountMap,
+		ActionTaken: true,
+	}
 }
 
 // PaySpeaker pays the speaker a salary.
-// OPTIONAL: override to pay the Speaker less than the full amount.
 
 //For this, whenever we are presidents, the salary of the speaker comes in hand with our own wealth state
 //this is for the sake of everyone paying less, thus having a higher chance of our island to recover
-func (pres *president) PaySpeaker(salary shared.Resources) (shared.Resources, bool) {
+func (pres *president) PaySpeaker(salary shared.Resources) shared.PresidentReturnContent {
 	if pres.c.wealth() == jeffBezos {
-		return salary, true
+		return shared.PresidentReturnContent{
+			ContentType:   shared.PresidentSpeakerSalary,
+			SpeakerSalary: salary,
+			ActionTaken:   true,
+		}
 	} else if pres.c.wealth() == middleClass {
 		salary = salary * 0.8
-		return salary, true
 	} else {
 		salary = salary * 0.5
-		return salary, true
+	}
+
+	return shared.PresidentReturnContent{
+		ContentType:   shared.PresidentSpeakerSalary,
+		SpeakerSalary: salary,
+		ActionTaken:   true,
 	}
 }
 
@@ -123,7 +146,7 @@ func (pres *president) CallSpeakerElection(monitoring shared.MonitorResult, turn
 // DecideNextSpeaker returns the ID of chosen next Speaker
 // OPTIONAL: override to manipulate the result of the election
 
-//Spicing things up and randomly choosing the speaker
+//Spicing things up and randomly choosing the speaker, however we will link this to opinion :)
 func (pres *president) DecideNextSpeaker(winner shared.ClientID) shared.ClientID {
 	rand.Seed(time.Now().UnixNano())
 	return shared.ClientID(rand.Intn(5))
