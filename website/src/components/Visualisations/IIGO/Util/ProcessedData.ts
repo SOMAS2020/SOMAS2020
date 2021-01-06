b9cc1000f10551d9677f0e75388e2649a230b277
Chi Keen Tan <devchikeen@gmail.com>
2021-01-06 20:07:25 +0000

    Add Basic Rules Visualization

---
+++ website/src/components/Visualisations/IIGO/Util/ProcessedData.ts
@@ -0,0 +1,49 @@
+import { OutputJSONType } from "../../../../consts/types";
+
+
+export type RuleType = {
+    ruleName: string;
+    mutable: boolean;
+    linked: boolean;
+    variables: string[];
+    history: {season: number; turn: number}[];
+};
+
+export const processRulesData = (data: OutputJSONType) :RuleType[]=> {
+    if (data.GameStates.length === 0) return []
+
+    // return CurrentRulesInPlay keys in term of seasons
+    let rulesInSeasons = data.GameStates.map((episode) => {
+        return {
+            season: episode.Season,
+            turn: episode.Turn,
+            rules: Object.keys(episode.CurrentRulesInPlay)
+        }
+    })
+
+    // return a list of rules of RuleType
+    let rulesDict:any = {}
+    // each season, do...
+    data.GameStates.map((episode) => {
+        // each rules in season, do...
+        Object.keys(episode.CurrentRulesInPlay).map((rules) => {
+            // add history
+            let history_:any = []
+            rulesInSeasons.forEach((item) => {
+                if (item.rules.includes(rules)){
+                    history_.push({season:item.season, turn:item.turn})
+                }
+            })
+            rulesDict[rules] = {
+                ruleName: episode.CurrentRulesInPlay[rules].RuleName,
+                mutable: episode.CurrentRulesInPlay[rules].Mutable,
+                linked: episode.CurrentRulesInPlay[rules].Linked,
+                variables: episode.CurrentRulesInPlay[rules].RequiredVariables,
+                history: history_,
+            }
+        })
+    })
+
+    return Object.values(rulesDict);
+}
+
