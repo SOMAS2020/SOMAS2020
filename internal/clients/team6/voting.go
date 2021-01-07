86bb0dd6088e0176abc56faddcb08ac6975f672d
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2021-01-07 01:56:01 +0000

    Update voting

--- internal/clients/team6/voting.go
+++ internal/clients/team6/voting.go
@@ -14,6 +14,11 @@ func (c *client) VoteForRule(ruleMatrix rules.RuleMatrix) shared.RuleVoteType {
 // VoteForElection returns the client's Borda vote for the role to be elected.
 func (c *client) VoteForElection(roleToElect shared.Role, candidateList []shared.ClientID) []shared.ClientID {
 	//Sort candidates according to friendship level as a preference list
+	//Every Island votes for itself first
+	friendship[id] = 0
+	for _, candidateID := range candidateList {
+		friendship[id] += friendship[candidateID]
+	}
 	for i := 0; i < len(candidateList); i++ {
 		for j := i; j < len(candidateList); j++ {
 			if friendship[candidateList[j]] > friendship[candidateList[i]] {
