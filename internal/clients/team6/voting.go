b742fa4a043f4b5356736879dcbfb75f8ab0ad6a
Diregie-J <74107336+Diregie-J@users.noreply.github.com>
2021-01-07 01:18:00 +0000

    Update voting.go

--- internal/clients/team6/voting.go
+++ internal/clients/team6/voting.go
@@ -5,14 +5,13 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
-// GetVoteForRule returns the client's vote in favour of or against a rule.
+// VoteForRule returns the client's vote in favour of or against a rule.
 func (c *client) VoteForRule(ruleMatrix rules.RuleMatrix) shared.RuleVoteType {
 	// TODO implement decision on voting that considers the rule
 	return shared.Abstain
 }
 
-// GetVoteForElection returns the client's Borda vote for the role to be elected.
-// COMPULSORY: use opinion formation to decide a rank for islands for the role
+// VoteForElection returns the client's Borda vote for the role to be elected.
 func (c *client) VoteForElection(roleToElect shared.Role, candidateList []shared.ClientID) []shared.ClientID {
 	candidates := map[int]shared.ClientID{}
 	for i := 0; i < len(candidateList); i++ {
