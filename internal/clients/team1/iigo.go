aed9eb2d24b42f537bafc9dd7e5566b2e8a07489
Michalis Pardalos <m.pardalos@gmail.com>
2021-01-08 01:41:02 +0200

    Add tax policy to agent

--- internal/clients/team1/iigo.go
+++ internal/clients/team1/iigo.go
@@ -5,6 +5,53 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
+/**********************/
+/*    Presidency      */
+/**********************/
+
+func (c *client) SetTaxationAmount(islandsResources map[shared.ClientID]shared.ResourcesReport) shared.PresidentReturnContent {
+	taxAmountMap := make(map[shared.ClientID]shared.Resources)
+
+	for clientID, clientReport := range islandsResources {
+		if !clientReport.Reported {
+			// If they are not reporting their wealth, they will probably also
+			// not pay tax.
+			taxAmountMap[clientID] = 9999999
+			c.addToOpinion(clientID, -1)
+			continue
+		}
+
+		taxRate := 0.0 // Percentage of their resources the client will be taxed
+		resources := clientReport.ReportedAmount
+		livingCost := c.BaseClient.ServerReadHandle.GetGameConfig().CostOfLiving
+
+		switch {
+		case resources < 2*livingCost:
+			taxRate = 0
+		case resources < 10*livingCost:
+			taxRate = 0.05
+		case resources < 50*livingCost:
+			taxRate = 0.3
+		case resources < 100*livingCost:
+			taxRate = 0.4
+		default:
+			taxRate = 0.6
+		}
+
+		// https://bit.ly/3s7dRXt
+		taxAmountMap[clientID] = shared.Resources(float64(clientReport.ReportedAmount) * taxRate)
+	}
+	return shared.PresidentReturnContent{
+		ContentType: shared.PresidentTaxation,
+		ResourceMap: taxAmountMap,
+		ActionTaken: true,
+	}
+}
+
+/*************************/
+/* Taxes and allocations */
+/*************************/
+
 func (c *client) GetTaxContribution() shared.Resources {
 	contribution, success := c.GetRecommendation(rules.IslandTaxContribution)
 	if !success {
