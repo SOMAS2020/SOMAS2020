fd862cc78da857326a539a88485573eb2902104f
Olly Larkin <ollylemonlarkin@gmail.com>
2021-01-08 16:01:26 +0000

    [VIS] some final styling

--- website/src/components/Visualisations/Achievements/Achievements.tsx
+++ website/src/components/Visualisations/Achievements/Achievements.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import logo from '../../../assets/logo/logo512.png'
 import styles from './Achievements.module.css'
 import { OutputJSONType } from '../../../consts/types'
@@ -9,22 +10,40 @@ import acheivementList, {
 
 type AchievementBarProps = {
   title: string
-  desc: string | unknown
+  desc: string
   winArr: TeamName[]
 }
 
 const IndivAchievement = ({ title, desc, winArr }: AchievementBarProps) => {
+  const winners =
+    winArr.length === 6 || winArr.length === 0
+      ? 'No winners :('
+      : winArr.join(', ')
+
   return (
-    <p>
-      {title}: {winArr.join(',')}
-    </p>
+    <div className={styles.achieveContainer}>
+      <Container className={styles.innerContainer}>
+        <Row>
+          <Col className={styles.leftColumn}>
+            <h4 style={{ textAlign: 'left' }}>{title}</h4>
+            <p style={{ textAlign: 'left' }}>{desc}</p>
+          </Col>
+          <Col />
+          <Col className={styles.rightColumn}>
+            <p style={{ textAlign: 'right' }}>{winners}</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
 const Achievements = (props: { output: OutputJSONType }) => {
   return (
     <div className={styles.root}>
-      <p className={styles.text}>Achievements Visualisation</p>
+      <p className={styles.text} style={{ marginBottom: 30 }}>
+        Achievements
+      </p>
       {acheivementList.map((achievement) => (
         <IndivAchievement
           key={achievement.title}
