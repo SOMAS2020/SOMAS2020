100eaca1b8796889591f18d51253c2595dcbac19
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-01-09 00:17:04 +0000

    Patch team 6 judge (Array not initialised)

--- internal/clients/team6/judge.go
+++ internal/clients/team6/judge.go
@@ -16,6 +16,7 @@ func (j *judge) GetPardonedIslands(currentSanctions map[int][]shared.Sanction) m
 
 	for timeStep, sanctions := range currentSanctions {
 		for who, sanction := range sanctions {
+			pardons[timeStep] = initialiseBoolArray(len(sanctions))
 			if timeStep == maxSanctionTime && j.client.friendship[sanction.ClientID] == j.client.clientConfig.maxFriendship {
 				// we can pardon certain islands having maximum friendship with us
 				pardons[timeStep][who] = true
@@ -24,4 +25,12 @@ func (j *judge) GetPardonedIslands(currentSanctions map[int][]shared.Sanction) m
 	}
 
 	return pardons
+}
+
+func initialiseBoolArray(length int) []bool {
+	ret := []bool{}
+	for i := 0; i < length; i++ {
+		ret = append(ret, false)
+	}
+	return ret
 }
