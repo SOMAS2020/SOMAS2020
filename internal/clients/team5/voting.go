1887f6ba2e27d21a6ce21166a4439f0b31459d6e
Anh Nguyen <50520772+anhnguyen24@users.noreply.github.com>
2021-01-09 02:39:57 +0000

    fix voting algorithm oops..

--- internal/clients/team5/voting.go
+++ internal/clients/team5/voting.go
@@ -9,36 +9,57 @@ import (
 // Generate borda vote based on opinion score for roles
 // Team 5 will always be top
 func (c *client) VoteForElection(roleToElect shared.Role, candidateList []shared.ClientID) []shared.ClientID {
-	opinionSortByTeam := c.opinionSortByTeam()
-	opinionSortByScore := c.opinionSortByScore()
-	ballot := c.sortedMapOfOpinion(findIndexOfScore(opinionSortByScore, opinionSortByTeam))
+	var refinedCandidateList []shared.ClientID
+	var intRefinedCandidateList []int
+	// Take out our ID
+	for _, islandID := range candidateList {
+		if islandID != shared.Team5 && islandID != shared.Team3 {
+			refinedCandidateList = append(refinedCandidateList, islandID)
+		}
+	}
+	// translate to int
+	for _, islandID := range refinedCandidateList {
+		intRefinedCandidateList = append(intRefinedCandidateList, int(islandID))
+	}
+	opinionSortByTeam := c.opinionSortByTeam(intRefinedCandidateList)
+	opinionSortByScore := c.opinionSortByTeam(intRefinedCandidateList)
+	opinionSortByScore = c.opinionSortByScore(opinionSortByScore)
+	ballot := c.sortedMapOfOpinion(c.findIndexOfScore(opinionSortByScore, opinionSortByTeam, intRefinedCandidateList), intRefinedCandidateList)
+	//last one
+	for _, islandID := range candidateList {
+		if islandID == shared.Team3 {
+			ballot = append(ballot, shared.Team3)
+		}
+	}
 	c.Logf("[DEBUG] - Ballot: %v", ballot)
+
 	return ballot
 }
 
 // opinion score in order of team number 1,2,3,4,5,6
-func (c *client) opinionSortByTeam() (opnionSortByTeam []opinionScore) {
-	opnionSortByTeam = append(opnionSortByTeam, c.opinions[shared.Team1].getScore(), c.opinions[shared.Team2].getScore())
-	opnionSortByTeam = append(opnionSortByTeam, c.opinions[shared.Team3].getScore(), c.opinions[shared.Team4].getScore())
-	opnionSortByTeam = append(opnionSortByTeam, c.opinions[shared.Team5].getScore(), c.opinions[shared.Team6].getScore())
-	return
+func (c *client) opinionSortByTeam(candidateList []int) (opinionSortByTeam []float64) {
+	sort.Ints(candidateList)
+	for _, island := range candidateList {
+		opinionSortByTeam = append(opinionSortByTeam, float64(c.opinions[shared.ClientID(island)].getScore()))
+	}
+	return opinionSortByTeam
 }
 
 //opinion sorted by scores from min to max
-func (c *client) opinionSortByScore() (opinionSortByScore []opinionScore) {
-	opinionSortByScore = c.opinionSortByTeam()
-	sort.Slice(opinionSortByScore, func(i, j int) bool {
-		return opinionSortByScore[i] < opinionSortByScore[j]
-	})
+func (c *client) opinionSortByScore(opinionSortByTeam []float64) (opinionSortByScore []float64) {
+	opinionSortByScore = opinionSortByTeam
+	sort.Float64s(opinionSortByScore)
 	return
 }
 
 // arrange teams corresponding to their opinion score from max -> min
-func findIndexOfScore(opinionSortByScore []opinionScore, opinionSortByTeam []opinionScore) (rank []int) {
+func (c *client) findIndexOfScore(opinionSortByScore []float64, opinionSortByTeam []float64, candidateList []int) (rank []int) {
 	for i := len(opinionSortByScore) - 1; i >= 0; i-- {
+		//for i := 0; i < len(opinionSortByScore); i++ {
 		for j := len(opinionSortByScore) - 1; j >= 0; j-- {
-			if opinionSortByTeam[j] == opinionSortByScore[i] {
-				rank = append(rank, j)
+			//for j := 0; j < len(opinionSortByScore); j++ {
+			if opinionSortByScore[i] == opinionSortByTeam[j] {
+				rank = append(rank, candidateList[j])
 				opinionSortByTeam[j] = 100 // assign random number we know we won't have so it won't mixed up the index
 			}
 		}
@@ -47,20 +68,21 @@ func findIndexOfScore(opinionSortByScore []opinionScore, opinionSortByTeam []opi
 }
 
 // translate int to shared.clientID but put our ID first and someone last
-func (c *client) sortedMapOfOpinion(rank []int) (sortedTeamByOpinion []shared.ClientID) {
-	sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team5)
-	for i := 0; i < len(rank); i++ {
-		if rank[i] == 0 {
-			sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team1)
-		} else if rank[i] == 1 {
-			sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team2)
-		} else if rank[i] == 3 {
-			sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team4)
-		} else if rank[i] == 5 {
-			sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team6)
+// assume that we are always alive when this function is called
+func (c *client) sortedMapOfOpinion(rank []int, candidateList []int) (sortedTeamByOpinion []shared.ClientID) {
+	var sortedCandidateList []int
+	allIsland := []int{0, 1, 2, 3, 4, 5}
+	for _, island := range allIsland {
+		for _, clientID := range candidateList {
+			if clientID == island && clientID != 4 && clientID != 2 {
+				sortedCandidateList = append(sortedCandidateList, island)
+			}
 		}
 	}
-	sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team3)
+	sortedTeamByOpinion = append(sortedTeamByOpinion, shared.Team5)
+	for _, clientID := range rank {
+		sortedTeamByOpinion = append(sortedTeamByOpinion, shared.ClientID(clientID))
+	}
 	return sortedTeamByOpinion
 }
 
