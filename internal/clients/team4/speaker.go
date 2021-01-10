85d3554764d327165818da91eee0b3b4cefb90b7
James Kim <777jameskim@gmail.com>
2021-01-10 21:36:11 +0000

    take actions based on budget+priorities

--- internal/clients/team4/speaker.go
+++ internal/clients/team4/speaker.go
@@ -11,12 +11,19 @@ type speaker struct {
 	parent *client
 }
 
+//SpeakerActionOrder indicates speaker actions in order of execution
+var SpeakerActionOrder = []string{
+	"SetRuleToVote",
+	"SetVotingResult",
+	"AnnounceVotingResult",
+	"AppointNextJudge",
+}
+
 //SpeakerActionPriorities indicate speaker actions in order of priority
 var SpeakerActionPriorities = []string{
 	"SetVotingResult",
-	"SetRuleToVote",
 	"AnnounceVotingResult",
-	"UpdateRules",
+	"SetRuleToVote",
 	"AppointNextJudge",
 }
 
@@ -41,13 +48,31 @@ func (s *speaker) getActionsCost(actions []string) shared.Resources {
 }
 
 func (s *speaker) getHigherPriorityActionsCost(baseaction string) shared.Resources {
-	actionindex := len(SpeakerActionPriorities)
-	for i, action := range SpeakerActionPriorities {
+	actionindex := len(SpeakerActionOrder)
+	for i, action := range SpeakerActionOrder {
 		if action == baseaction {
 			actionindex = i
 		}
 	}
-	return s.getActionsCost(SpeakerActionPriorities[:actionindex])
+	priorityindex := len(SpeakerActionPriorities)
+	for i, action := range SpeakerActionPriorities {
+		if action == baseaction {
+			priorityindex = i
+		}
+	}
+	var SAPcopy []string = SpeakerActionPriorities[:0]
+	for _, action1 := range SpeakerActionPriorities[:priorityindex] {
+		alreadyExecuted := false
+		for _, action2 := range SpeakerActionOrder[:actionindex] {
+			if action1 == action2 {
+				alreadyExecuted = true
+			}
+		}
+		if !alreadyExecuted {
+			SAPcopy = append(SAPcopy, action1)
+		}
+	}
+	return s.getActionsCost(SAPcopy)
 }
 
 // PayJudge is used for paying judge for his service
@@ -67,6 +92,14 @@ func (s *speaker) PayJudge() shared.SpeakerReturnContent {
 //DecideAgenda the interface implementation and example of a well behaved Speaker
 //who sets the vote to be voted on to be the rule the President provided
 func (s *speaker) DecideAgenda(ruleMatrix rules.RuleMatrix) shared.SpeakerReturnContent {
+	//there are more important things to do...
+	s.parent.Logf("Action costs: %v, %v, %v, %v", s.getActionsCost([]string{"SetRuleToVote"}), s.getActionsCost([]string{"SetVotingResult"}), s.getActionsCost([]string{"AnnounceVotingResult"}), s.getActionsCost([]string{"AppointNextJudge"}))
+	s.parent.Logf("Higher priority: %v, %v, %v, %v", s.getHigherPriorityActionsCost("SetRuleToVote"), s.getHigherPriorityActionsCost("SetVotingResult"), s.getHigherPriorityActionsCost("AnnounceVotingResult"), s.getHigherPriorityActionsCost("AppointNextJudge"))
+	if s.getSpeakerBudget() < s.getHigherPriorityActionsCost("SetRuleToVote") {
+		return shared.SpeakerReturnContent{
+			ActionTaken: false,
+		}
+	}
 	if ruleMatrix.RuleMatrixIsEmpty() {
 		//president has not selected a rule
 	}
@@ -81,6 +114,12 @@ func (s *speaker) DecideAgenda(ruleMatrix rules.RuleMatrix) shared.SpeakerReturn
 //who calls a vote on the proposed rule and asks all available islands to vote.
 //Return an empty string or empty []shared.ClientID for no vote to occur
 func (s *speaker) DecideVote(ruleMatrix rules.RuleMatrix, aliveClients []shared.ClientID) shared.SpeakerReturnContent {
+	//there are more important things to do...
+	if s.getSpeakerBudget() < s.getHigherPriorityActionsCost("SetVotingResult") {
+		return shared.SpeakerReturnContent{
+			ActionTaken: false,
+		}
+	}
 	return shared.SpeakerReturnContent{
 		ContentType:          shared.SpeakerVote,
 		ParticipatingIslands: aliveClients,
@@ -93,6 +132,12 @@ func (s *speaker) DecideVote(ruleMatrix rules.RuleMatrix, aliveClients []shared.
 //A well behaved speaker announces what had been voted on and the corresponding result
 //Return "", _ for no announcement to occur
 func (s *speaker) DecideAnnouncement(ruleMatrix rules.RuleMatrix, result bool) shared.SpeakerReturnContent {
+	//there are more important things to do...
+	if s.getSpeakerBudget() < s.getHigherPriorityActionsCost("AnnounceVotingResult") {
+		return shared.SpeakerReturnContent{
+			ActionTaken: false,
+		}
+	}
 	return shared.SpeakerReturnContent{
 		ContentType:  shared.SpeakerAnnouncement,
 		RuleMatrix:   ruleMatrix,
@@ -110,6 +155,10 @@ func (s *speaker) CallJudgeElection(monitoring shared.MonitorResult, turnsInPowe
 		VotingMethod:  shared.BordaCount,
 		IslandsToVote: allIslands,
 		HoldElection:  false,
+	}
+	//there are more important things to do...
+	if s.getSpeakerBudget() < s.getHigherPriorityActionsCost("AppointNextJudge") {
+		return electionsettings
 	}
 	if monitoring.Performed && !monitoring.Result {
 		electionsettings.HoldElection = true
