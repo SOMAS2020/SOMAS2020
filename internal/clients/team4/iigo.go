640fc0e56c165c6ea610f5b4d27afd74b0707537
zx516 <zx516@imperial.ac.uk>
2021-01-11 04:14:45 +0000

    structure change

--- internal/clients/team4/iigo.go
+++ internal/clients/team4/iigo.go
@@ -49,19 +49,11 @@ func (c *client) RequestAllocation() shared.Resources {
 	})
 
 	uncomplianceLevel := mat.Dot(importance, parameters) - uncomplianceThreshold
-	// TODO: if we're in critical state, take the resource needed to get us out of it. Maybe take a protion of what we need
-	// until the very last turns in which we're about to die and take all we need to get out of critical state.
-	// c.ServerReadHandle.GetGameConfig().maxCriticalConsecutiveTurns
+
+	// if alive and compliant then take nothing if granted nothing
 	allocDemanded := allocationGranted
 	resNeeded := c.ServerReadHandle.GetGameConfig().MinimumResourceThreshold + c.ServerReadHandle.GetGameConfig().CostOfLiving - c.getOurResources()
-	if uncomplianceLevel > 0 {
-		if allocationGranted == 0 {
-			c.internalParam.giftExtra = false
-			allocDemanded = shared.Resources(math.Min(float64(resNeeded*2), float64(commonPool))) * shared.Resources((uncomplianceLevel + 1))
-		} else {
-			allocDemanded = allocationGranted * shared.Resources((uncomplianceLevel + 1))
-		}
-	}
+
 	if ourLifeStatus == shared.Critical {
 		c.internalParam.giftExtra = false
 		maxTurnsInCritical := c.ServerReadHandle.GetGameConfig().MaxCriticalConsecutiveTurns
@@ -77,6 +69,15 @@ func (c *client) RequestAllocation() shared.Resources {
 	} else if ourLifeStatus == shared.Dead {
 		c.internalParam.giftExtra = false
 		allocDemanded = shared.Resources(0)
+	} else if ourLifeStatus == shared.Alive {
+		if uncomplianceLevel > 0 {
+			if allocationGranted == 0 {
+				c.internalParam.giftExtra = false
+				allocDemanded = shared.Resources(math.Min(float64(resNeeded*2), float64(commonPool))) * shared.Resources((uncomplianceLevel + 1))
+			} else {
+				allocDemanded = allocationGranted * shared.Resources((uncomplianceLevel + 1))
+			}
+		}
 	}
 
 	return allocDemanded
