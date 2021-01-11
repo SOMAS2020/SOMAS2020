db0848779eb16bd9f90da97d6aa3fdbdeddefa40
Andrzej Kowalewski <37296984+akowal3@users.noreply.github.com>
2021-01-11 11:41:43 +0100

    the test actually tests something

--- internal/clients/team4/client_test.go
+++ internal/clients/team4/client_test.go
@@ -19,56 +19,70 @@ func TestGetJudgePointer(t *testing.T) {
 	}
 }
 
-// func TestUpdateTrustFromHistory(t *testing.T) {
-// 	cases := []struct {
-// 		name         string
-// 		savedHistory accountabilityHistory
-// 	}{
-// 		{
-// 			name: "simple test",
-// 			savedHistory: accountabilityHistory{
-// 				history: map[uint]map[shared.ClientID]judgeHistoryInfo{
-// 					1: {
-// 						shared.Team1: {
-// 							TruthfulRatio: 0.2,
-// 						},
-// 						shared.Team2: {
-// 							TruthfulRatio: 0.4,
-// 						},
-// 						shared.Team3: {
-// 							TruthfulRatio: 0.87,
-// 						},
-// 						shared.Team4: {
-// 							TruthfulRatio: 1,
-// 						},
-// 						shared.Team5: {
-// 							TruthfulRatio: 0.6319,
-// 						},
-// 						shared.Team6: {
-// 							TruthfulRatio: 0.55,
-// 						},
-// 					},
-// 				},
-// 				updated:     true,
-// 				updatedTurn: 1,
-// 			},
-// 		},
-// 	}
+func TestUpdateTrustFromHistory(t *testing.T) {
+	cases := []struct {
+		name         string
+		savedHistory accountabilityHistory
+	}{
+		{
+			name: "simple test",
+			savedHistory: accountabilityHistory{
+				history: map[uint]map[shared.ClientID]judgeHistoryInfo{
+					1: {
+						shared.Team1: {
+							LawfulRatio: 0.2,
+						},
+						shared.Team2: {
+							LawfulRatio: 0.4,
+						},
+						shared.Team3: {
+							LawfulRatio: 0.87,
+						},
+						shared.Team4: {
+							LawfulRatio: 1,
+						},
+						shared.Team5: {
+							LawfulRatio: 0.675,
+						},
+						shared.Team6: {
+							LawfulRatio: 0.55,
+						},
+					},
+				},
+				updated:     true,
+				updatedTurn: 1,
+			},
+		},
+	}
 
-// 	for _, tc := range cases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			clients := []shared.ClientID{}
-// 			for clientID := range tc.savedHistory.getNewInfo() {
-// 				clients = append(clients, clientID)
-// 			}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			clients := []shared.ClientID{}
+			for clientID := range tc.savedHistory.getNewInfo() {
+				clients = append(clients, clientID)
+			}
 
-// 			testServer := fakeServerHandle{clients: clients}
-// 			testClient := newClientInternal(id, t)
-// 			testClient.Initialise(testServer)
+			testServer := fakeServerHandle{clients: clients}
+			testClient := newClientInternal(id)
+			testClient.Initialise(testServer)
 
-// 			testClient.savedHistory = &tc.savedHistory
-// 			testClient.updateTrustFromSavedHistory()
+			testClient.savedHistory = &tc.savedHistory
+			testClient.updateTrustFromSavedHistory()
 
-// 		})
-// 	}
-// }
+			for clientID, trust := range testClient.trustMatrix.trustMap {
+				// t.Logf("Updated trust for %v: %v", clientID, trust)
+				if floatEqual(trust, 0.5) {
+					t.Errorf("Update from history failed. trust didn't change for %v", clientID)
+				}
+			}
+
+			if trustSum := testClient.trustMatrix.totalTrustSum(); trustSum > 0 {
+				averageTrust := trustSum / float64(len(testClient.trustMatrix.trustMap))
+				if !floatEqual(averageTrust, 0.5) {
+					t.Errorf("Update from history failed. Average trust: %v", averageTrust)
+				}
+			}
+
+		})
+	}
+}
