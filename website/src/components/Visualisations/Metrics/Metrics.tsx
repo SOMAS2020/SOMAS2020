b58d064d7ace1011f3b7e34e7d641b1814791689
Yannis Panagis <yannis.panagis1998@gmail.com>
2021-01-12 18:43:17 +0000

    converted metrics summary to table

--- website/src/components/Visualisations/Metrics/Metrics.tsx
+++ website/src/components/Visualisations/Metrics/Metrics.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { IconButton, List, ListItemText, Collapse } from '@material-ui/core'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
 import confetti from 'canvas-confetti'
 import styles from './Metrics.module.css'
 import { OutputJSONType } from '../../../consts/types'
-import metricList, { evaluateMetrics, TeamName, Metric } from './MetricEntries'
+import metricList, { evaluateMetrics, Metric } from './MetricEntries'
 
 type MetricBarProps = {
   title: string
@@ -14,70 +14,36 @@ type MetricBarProps = {
   metrics: Metric[]
 }
 
-const IndivMetric = ({ title, desc, metrics }: MetricBarProps) => {
-  const [open, setOpen] = React.useState(false)
-
-  function handleAchievementClick() {
-    confetti({
-      particleCount: 300,
-      spread: 100,
-      origin: { y: 0.6 },
-    })
-  }
-
-  return (
-    <div className={styles.metricContainer}>
-      <Container
-        className={styles.innerContainer}
-        onClick={handleAchievementClick}
-      >
-        <Row>
-          <Col className={styles.leftColumn}>
-            <h4 style={{ textAlign: 'left' }}>{title}</h4>
-            <p style={{ textAlign: 'left' }}>{desc}</p>
-          </Col>
-        </Row>
-        <IconButton
-          aria-label="expand row"
-          size="small"
-          onClick={() => setOpen(!open)}
-        >
-          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-        </IconButton>
-        <Row>
-          <Col className={styles.centerColumn}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <List component="nav">
-                {metrics.map((metric) => (
-                  <ListItemText
-                    primary={[metric.teamName, ': ', metric.value.toFixed(2)]}
-                    key={metric.teamName.toString()}
-                  />
-                ))}
-              </List>
-            </Collapse>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  )
-}
-
 const Metrics = (props: { output: OutputJSONType }) => {
   return (
-    <div className={styles.root}>
-      <p className={styles.text} style={{ marginBottom: 30 }}>
-        Metrics
-      </p>
-      {metricList.map((metric) => (
-        <IndivMetric
-          key={metric.title}
-          title={metric.title}
-          desc={metric.description}
-          metrics={evaluateMetrics(props.output, metric)}
-        />
-      ))}
-    </div>
+    <>
+      <h2 className={styles.text}>Metrics Summary</h2>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <th>Metric</th>
+          <th>Description</th>
+          <th>Team 1</th>
+          <th>Team 2</th>
+          <th>Team 3</th>
+          <th>Team 4</th>
+          <th>Team 5</th>
+          <th>Team 6</th>
+        </thead>
+        <tbody>
+          {metricList.map((metric) => (
+            <tr>
+              <td style={{ textAlign: 'left' }}>{metric.title}</td>
+              <td style={{ textAlign: 'left' }}>{metric.description}</td>
+              {evaluateMetrics(props.output, metric).map((team) => (
+                <td style={{ textAlign: 'right' }} key={team.value.toString()}>
+                  {team.value.toFixed(2).toString()}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   )
 }
 
