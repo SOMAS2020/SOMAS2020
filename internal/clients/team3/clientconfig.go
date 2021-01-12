bd91b8d0d0e543e549afa47571f6fb10798cf43e
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-01-12 15:29:33 +0000

    refactor client config for convenience

--- internal/clients/team3/clientconfig.go
+++ internal/clients/team3/clientconfig.go
@@ -3,13 +3,49 @@ package team3
 import "github.com/SOMAS2020/SOMAS2020/internal/clients/team3/adv"
 
 func getislandParams() islandParams {
+	return ardernLeeMacron()
+}
+
+func evilPeople() islandParams {
 	return islandParams{
-		equity:                  1.1,    // 0-1 // do we need this or can it be replaced?
-		complianceLevel:         0.75,  // 0-1 //this seems good
-		resourcesSkew:           1.5,    // >1 //same as equity
+		equity:                  1,     // 0-1 // do we need this or can it be replaced?
+		complianceLevel:         0.5,   // 0-1 //this seems good
+		resourcesSkew:           2,     // >1 //same as equity
+		saveCriticalIsland:      false, //seems like this will always be true
+		selfishness:             1.0,   //0-1 //higher is better for us
+		riskFactor:              0.8,   // 0-1 // increasing this has mixed results
+		friendliness:            1,
+		advType:                 adv.MaliceAdv, // 0-1 // agent performs better when this matches selfishness
+		giftInflationPercentage: 1,             // 0-1 //this doesn't have a noticeable effect
+		sensitivity:             0.5,           // 0-1
+		controlLoop:             true,
+	}
+}
+
+func gandhi() islandParams {
+	return islandParams{
+		equity:                  1.2,  // 0-1 // do we need this or can it be replaced?
+		complianceLevel:         1,    // 0-1 //this seems good
+		resourcesSkew:           1,    // >1 //same as equity
 		saveCriticalIsland:      true, //seems like this will always be true
-		selfishness:             0.75,    //0-1 //higher is better for us
-		riskFactor:              0.125,  // 0-1 // increasing this has mixed results
+		selfishness:             0.5,  //0-1 //higher is better for us
+		riskFactor:              0.05, // 0-1 // increasing this has mixed results
+		friendliness:            1,
+		advType:                 adv.NoAdv, // 0-1 // agent performs better when this matches selfishness
+		giftInflationPercentage: 1,         // 0-1 //this doesn't have a noticeable effect
+		sensitivity:             0.5,       // 0-1
+		controlLoop:             false,
+	}
+}
+
+func ardernLeeMacron() islandParams {
+	return islandParams{
+		equity:                  1.1,   // 0-1 // do we need this or can it be replaced?
+		complianceLevel:         0.75,  // 0-1 //this seems good
+		resourcesSkew:           1.5,   // >1 //same as equity
+		saveCriticalIsland:      true,  //seems like this will always be true
+		selfishness:             0.75,  //0-1 //higher is better for us
+		riskFactor:              0.125, // 0-1 // increasing this has mixed results
 		friendliness:            1,
 		advType:                 adv.NoAdv, // 0-1 // agent performs better when this matches selfishness
 		giftInflationPercentage: 1,         // 0-1 //this doesn't have a noticeable effect
@@ -17,9 +53,3 @@ func getislandParams() islandParams {
 		controlLoop:             true,
 	}
 }
-
-// TODOS:
-// 1. finalize the list of parameters.
-// 2. pick 3 sets of values that correspond to different behaviours & submit to naim. play mainly with selfishness, risk, friendliness.
-// 3. Improve foraging if we can
-// 4. WE ARE NOT VOTING WELL! Add a sensitivity parameter and use lists changed by evalPerformance to vote
