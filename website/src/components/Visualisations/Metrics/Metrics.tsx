36eed81cac654e0800827fca057018bd31c0a7fb
Yannis Panagis <yannis.panagis1998@gmail.com>
2021-01-12 18:51:19 +0000

    removed unused imports and fixed console warnings

--- website/src/components/Visualisations/Metrics/Metrics.tsx
+++ website/src/components/Visualisations/Metrics/Metrics.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { IconButton, List, ListItemText, Collapse } from '@material-ui/core'
 import { Table } from 'react-bootstrap'
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
-import confetti from 'canvas-confetti'
 import styles from './Metrics.module.css'
 import { OutputJSONType } from '../../../consts/types'
 import metricList, { evaluateMetrics, Metric } from './MetricEntries'
-
-type MetricBarProps = {
-  title: string
-  desc: string
-  metrics: Metric[]
-}
 
 const Metrics = (props: { output: OutputJSONType }) => {
   return (
@@ -20,22 +10,28 @@ const Metrics = (props: { output: OutputJSONType }) => {
       <h2 className={styles.text}>Metrics Summary</h2>
       <Table striped bordered hover size="sm">
         <thead>
-          <th>Metric</th>
-          <th>Description</th>
-          <th>Team 1</th>
-          <th>Team 2</th>
-          <th>Team 3</th>
-          <th>Team 4</th>
-          <th>Team 5</th>
-          <th>Team 6</th>
+          <tr>
+            <th>Metric</th>
+            <th>Description</th>
+            <th>Team 1</th>
+            <th>Team 2</th>
+            <th>Team 3</th>
+            <th>Team 4</th>
+            <th>Team 5</th>
+            <th>Team 6</th>
+          </tr>
         </thead>
         <tbody>
           {metricList.map((metric) => (
-            <tr>
+            <tr key={metric.title}>
               <td style={{ textAlign: 'left' }}>{metric.title}</td>
               <td style={{ textAlign: 'left' }}>{metric.description}</td>
               {evaluateMetrics(props.output, metric).map((team) => (
-                <td style={{ textAlign: 'right' }} key={team.value.toString()}>
+                <td
+                  style={{ textAlign: 'right' }}
+                  // hacky but heyo if it works
+                  key={`${team.teamName}+${metric.title}`}
+                >
                   {team.value.toFixed(2).toString()}
                 </td>
               ))}
