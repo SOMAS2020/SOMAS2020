0900da8b7195e5f73ba40931bf56db31a18f88f3
Preet Lalli <preetlalli@gmail.com>
2021-01-12 12:33:58 +0000

    global morning

--- internal/server/iigointernal/monitoring.go
+++ internal/server/iigointernal/monitoring.go
@@ -3,9 +3,8 @@ package iigointernal
 import (
 	"fmt"
 
-	"github.com/SOMAS2020/SOMAS2020/internal/common/config"
-
 	"github.com/SOMAS2020/SOMAS2020/internal/common/baseclient"
+	"github.com/SOMAS2020/SOMAS2020/internal/common/config"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/gamestate"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
