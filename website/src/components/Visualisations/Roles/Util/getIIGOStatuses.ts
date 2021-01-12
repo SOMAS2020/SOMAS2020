5f5dc817d94077da4072a572dfcb8f6cb1c74e32
Ramon Jutaviriya <ramon15749@gmail.com>
2021-01-12 23:34:57 +0000

    clean up getIIGOStatuses

--- website/src/components/Visualisations/Roles/Util/getIIGOStatuses.ts
+++ website/src/components/Visualisations/Roles/Util/getIIGOStatuses.ts
@@ -1,19 +1,15 @@
 import { OutputJSONType } from '../../../../consts/types'
-import { IIGOInfos, IIGOInfo } from './RoleTypes'
+import { IIGOInfos } from './RoleTypes'
 
 export const getIIGOStatuses = (data: OutputJSONType): IIGOInfos => {
     if (data.GameStates.length <= 1) return []
 
-    const retData: IIGOInfos = []
-
-    return data.GameStates.slice(1).map<IIGOInfo>((gameState) => {
-        const info: IIGOInfo = {
+    return data.GameStates.slice(1).map((gameState) => {
+        return {
             turn: gameState.Turn - 1,
             status: gameState.IIGORunStatus,
         }
-
-        return info
-    }, retData)
+    })
 }
 
 export default getIIGOStatuses
